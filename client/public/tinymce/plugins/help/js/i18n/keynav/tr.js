69c42c96ec780ee6435df14a63126482771da264
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:56:41 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/tr.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.tr',
+'<h1>Klavyeyle gezintiyi baÅlatma</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>MenÃ¼ Ã§ubuÄuna odaklan</dt>\n' +
+  '  <dd>Windows veya Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>AraÃ§ Ã§ubuÄuna odaklan</dt>\n' +
+  '  <dd>Windows veya Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>Alt bilgiye odaklan</dt>\n' +
+  '  <dd>Windows veya Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>Bildirime odakla</dt>\n' +
+  '  <dd>Windows veya Linux: Alt+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>BaÄlamsal araÃ§ Ã§ubuÄuna odaklan</dt>\n' +
+  '  <dd>Windows, Linux veya macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>Gezinti ilk kullanÄ±cÄ± arabirimi Ã¶Äesinden baÅlar, bu Ã¶Äe vurgulanÄ±r ya da ilk Ã¶Äe, Alt bilgi elemanÄ±\n' +
+  '  yolundaysa altÄ± Ã§izilir.</p>\n' +
+  '\n' +
+  '<h1>KullanÄ±cÄ± arabirimi bÃ¶lÃ¼mleri arasÄ±nda gezinme</h1>\n' +
+  '\n' +
+  '<p>Sonraki kullanÄ±cÄ± arabirimi bÃ¶lÃ¼mÃ¼ne gitmek iÃ§in <strong>Sekme</strong> tuÅuna basÄ±n.</p>\n' +
+  '\n' +
+  '<p>Ãnceki kullanÄ±cÄ± arabirimi bÃ¶lÃ¼mÃ¼ne gitmek iÃ§in <strong>Shift+Sekme</strong> tuÅlarÄ±na basÄ±n.</p>\n' +
+  '\n' +
+  '<p>Bu kullanÄ±cÄ± arabirimi bÃ¶lÃ¼mlerinin <strong>Sekme</strong> sÄ±rasÄ±:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>MenÃ¼ Ã§ubuÄu</li>\n' +
+  '  <li>Her araÃ§ Ã§ubuÄu grubu</li>\n' +
+  '  <li>Kenar Ã§ubuÄu</li>\n' +
+  '  <li>Alt bilgide Ã¶Äe yolu</li>\n' +
+  '  <li>Alt bilgide sÃ¶zcÃ¼k sayÄ±sÄ± geÃ§iÅ dÃ¼Ämesi</li>\n' +
+  '  <li>Alt bilgide marka baÄlantÄ±sÄ±</li>\n' +
+  '  <li>Alt bilgide dÃ¼zenleyiciyi yeniden boyutlandÄ±rma tutamacÄ±</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>KullanÄ±cÄ± arabirimi bÃ¶lÃ¼mÃ¼ yoksa atlanÄ±r.</p>\n' +
+  '\n' +
+  '<p>Alt bilgide klavyeyle gezinti odaÄÄ± yoksa ve gÃ¶rÃ¼nÃ¼r bir kenar Ã§ubuÄu mevcut deÄilse <strong>Shift+Sekme</strong> tuÅlarÄ±na basÄ±ldÄ±ÄÄ±nda\n' +
+  '  odak son araÃ§ Ã§ubuÄu yerine ilk araÃ§ Ã§ubuÄu grubuna taÅÄ±nÄ±r.</p>\n' +
+  '\n' +
+  '<h1>KullanÄ±cÄ± arabirimi bÃ¶lÃ¼mleri iÃ§inde gezinme</h1>\n' +
+  '\n' +
+  '<p>Sonraki kullanÄ±cÄ± arabirimi elemanÄ±na gitmek iÃ§in uygun <strong>Ok</strong> tuÅuna basÄ±n.</p>\n' +
+  '\n' +
+  '<p><strong>Sol</strong> ve <strong>SaÄ</strong> ok tuÅlarÄ±</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>menÃ¼ Ã§ubuÄundaki menÃ¼ler arasÄ±nda hareket eder.</li>\n' +
+  '  <li>menÃ¼de bir alt menÃ¼ aÃ§ar.</li>\n' +
+  '  <li>araÃ§ Ã§ubuÄu grubundaki dÃ¼Ämeler arasÄ±nda hareket eder.</li>\n' +
+  '  <li>alt bilginin Ã¶Äe yolundaki Ã¶Äeler arasÄ±nda hareket eder.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p><strong>AÅaÄÄ±</strong> ve <strong>YukarÄ±</strong> ok tuÅlarÄ±</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>menÃ¼deki menÃ¼ Ã¶Äeleri arasÄ±nda hareket eder.</li>\n' +
+  '  <li>araÃ§ Ã§ubuÄu aÃ§Ä±lÄ±r menÃ¼sÃ¼ndeki Ã¶Äeler arasÄ±nda hareket eder.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p><strong>Ok</strong> tuÅlarÄ±, odaklanÄ±lan kullanÄ±cÄ± arabirimi bÃ¶lÃ¼mÃ¼ iÃ§inde dÃ¶ngÃ¼sel olarak hareket eder.</p>\n' +
+  '\n' +
+  '<p>AÃ§Ä±k bir menÃ¼yÃ¼, aÃ§Ä±k bir alt menÃ¼yÃ¼ veya aÃ§Ä±k bir aÃ§Ä±lÄ±r menÃ¼yÃ¼ kapatmak iÃ§in <strong>Esc</strong> tuÅuna basÄ±n.</p>\n' +
+  '\n' +
+  '<p>GeÃ§erli odak belirli bir kullanÄ±cÄ± arabirimi bÃ¶lÃ¼mÃ¼nÃ¼n "Ã¼st" kÄ±smÄ±ndaysa <strong>Esc</strong> tuÅuna basÄ±ldÄ±ÄÄ±nda\n' +
+  '  klavyeyle gezintiden de tamamen Ã§Ä±kÄ±lÄ±r.</p>\n' +
+  '\n' +
+  '<h1>MenÃ¼ Ã¶Äesini veya araÃ§ Ã§ubuÄu dÃ¼Ämesini yÃ¼rÃ¼tme</h1>\n' +
+  '\n' +
+  '<p>Ä°stediÄiniz menÃ¼ Ã¶Äesi veya araÃ§ Ã§ubuÄu dÃ¼Ämesi vurgulandÄ±ÄÄ±nda <strong>Return</strong>, <strong>Enter</strong>\n' +
+  '  veya <strong>Ara Ã§ubuÄu</strong> tuÅuna basÄ±n.</p>\n' +
+  '\n' +
+  '<h1>Sekme bulunmayan iletiÅim kutularÄ±nda gezinme</h1>\n' +
+  '\n' +
+  '<p>Sekme bulunmayan iletiÅim kutularÄ±nda, iletiÅim kutusu aÃ§Ä±ldÄ±ÄÄ±nda ilk etkileÅimli bileÅene odaklanÄ±lÄ±r.</p>\n' +
+  '\n' +
+  '<p>EtkileÅimli iletiÅim kutusu bileÅenleri arasÄ±nda gezinmek iÃ§in <strong>Sekme</strong> veya <strong>Shift+ Sekme</strong> tuÅlarÄ±na basÄ±n.</p>\n' +
+  '\n' +
+  '<h1>Sekmeli iletiÅim kutularÄ±nda gezinme</h1>\n' +
+  '\n' +
+  '<p>Sekmeli iletiÅim kutularÄ±nda, iletiÅim kutusu aÃ§Ä±ldÄ±ÄÄ±nda sekme menÃ¼sÃ¼ndeki ilk dÃ¼Ämeye odaklanÄ±lÄ±r.</p>\n' +
+  '\n' +
+  '<p>Bu iletiÅim kutusu sekmesinin etkileÅimli bileÅenleri arasÄ±nda gezinmek iÃ§in <strong>Sekme</strong> veya\n' +
+  '  <strong>Shift+Sekme</strong> tuÅlarÄ±na basÄ±n.</p>\n' +
+  '\n' +
+  '<p>Mevcut sekmeler arasÄ±nda geÃ§iÅ yapmak iÃ§in sekme menÃ¼sÃ¼ne odaklanÄ±p uygun <strong>Ok</strong> tuÅuna basarak\n' +
+  '  baÅka bir iletiÅim kutusu sekmesine geÃ§iÅ yapÄ±n.</p>\n');
