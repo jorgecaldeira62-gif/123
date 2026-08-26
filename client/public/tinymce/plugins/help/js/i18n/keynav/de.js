1aac016a35bb9b5f021ee86a32241ea48cfb7dc1
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:55:47 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/de.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.de',
+'<h1>Grundlagen der Tastaturnavigation</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>Fokus auf MenÃ¼leiste</dt>\n' +
+  '  <dd>Windows oder Linux: ALT+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>Fokus auf Symbolleiste</dt>\n' +
+  '  <dd>Windows oder Linux: ALT+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>Fokus auf FuÃzeile</dt>\n' +
+  '  <dd>Windows oder Linux: ALT+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>Benachrichtigung fokussieren</dt>\n' +
+  '  <dd>Windows oder Linux: ALT+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>Fokus auf kontextbezogene Symbolleiste</dt>\n' +
+  '  <dd>Windows, Linux oder macOS: STRG+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>Die Navigation beginnt beim ersten BenutzeroberflÃ¤chenelement, welches hervorgehoben ist. Falls sich das erste Element im Pfad der FuÃzeile befindet,\n' +
+  '  ist es unterstrichen.</p>\n' +
+  '\n' +
+  '<h1>Zwischen Abschnitten der BenutzeroberflÃ¤che navigieren</h1>\n' +
+  '\n' +
+  '<p>Um von einem Abschnitt der BenutzeroberflÃ¤che zum nÃ¤chsten zu wechseln, drÃ¼cken Sie <strong>TAB</strong>.</p>\n' +
+  '\n' +
+  '<p>Um von einem Abschnitt der BenutzeroberflÃ¤che zum vorherigen zu wechseln, drÃ¼cken Sie <strong>UMSCHALT+TAB</strong>.</p>\n' +
+  '\n' +
+  '<p>Die Abschnitte der BenutzeroberflÃ¤che haben folgende <strong>TAB</strong>-Reihenfolge:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>MenÃ¼leiste</li>\n' +
+  '  <li>Einzelne Gruppen der Symbolleiste</li>\n' +
+  '  <li>Randleiste</li>\n' +
+  '  <li>Elementpfad in der FuÃzeile</li>\n' +
+  '  <li>UmschaltflÃ¤che âWÃ¶rter zÃ¤hlenâ in der FuÃzeile</li>\n' +
+  '  <li>Branding-Link in der FuÃzeile</li>\n' +
+  '  <li>Editor-Ziehpunkt zur GrÃ¶ÃenÃ¤nderung in der FuÃzeile</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Falls ein Abschnitt der BenutzeroberflÃ¤chen nicht vorhanden ist, wird er Ã¼bersprungen.</p>\n' +
+  '\n' +
+  '<p>Wenn in der FuÃzeile die Tastaturnavigation fokussiert ist und keine Randleiste angezeigt wird, wechselt der Fokus durch DrÃ¼cken von <strong>UMSCHALT+TAB</strong>\n' +
+  '  zur ersten Gruppe der Symbolleiste, nicht zur letzten.</p>\n' +
+  '\n' +
+  '<h1>Innerhalb von Abschnitten der BenutzeroberflÃ¤che navigieren</h1>\n' +
+  '\n' +
+  '<p>Um von einem Element der BenutzeroberflÃ¤che zum nÃ¤chsten zu wechseln, drÃ¼cken Sie die entsprechende <strong>Pfeiltaste</strong>.</p>\n' +
+  '\n' +
+  '<p>Die Pfeiltasten <strong>Links</strong> und <strong>Rechts</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>wechseln zwischen MenÃ¼s in der MenÃ¼leiste.</li>\n' +
+  '  <li>Ã¶ffnen das UntermenÃ¼ eines MenÃ¼s.</li>\n' +
+  '  <li>wechseln zwischen SchaltflÃ¤chen in einer Gruppe der Symbolleiste.</li>\n' +
+  '  <li>wechseln zwischen Elementen im Elementpfad der FuÃzeile.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>Die Pfeiltasten <strong>AbwÃ¤rts</strong> und <strong>AufwÃ¤rts</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>wechseln zwischen MenÃ¼elementen in einem MenÃ¼.</li>\n' +
+  '  <li>wechseln zwischen Elementen in einem PopupmenÃ¼ der Symbolleiste.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>Die <strong>Pfeiltasten</strong> rotieren innerhalb des fokussierten Abschnitts der BenutzeroberflÃ¤che.</p>\n' +
+  '\n' +
+  '<p>Um ein geÃ¶ffnetes MenÃ¼, ein geÃ¶ffnetes UntermenÃ¼ oder ein geÃ¶ffnetes PopupmenÃ¼ zu schlieÃen, drÃ¼cken Sie die <strong>ESC</strong>-Taste.</p>\n' +
+  '\n' +
+  '<p>Wenn sich der aktuelle Fokus ganz oben in einem bestimmten Abschnitt der BenutzeroberflÃ¤che befindet, wird durch DrÃ¼cken der <strong>ESC</strong>-Taste auch\n' +
+  '  die Tastaturnavigation beendet.</p>\n' +
+  '\n' +
+  '<h1>Ein MenÃ¼element oder eine SymbolleistenschaltflÃ¤che ausfÃ¼hren</h1>\n' +
+  '\n' +
+  '<p>Wenn das gewÃ¼nschte MenÃ¼element oder die gewÃ¼nschte SymbolleistenschaltflÃ¤che hervorgehoben ist, drÃ¼cken Sie <strong>ZurÃ¼ck</strong>, <strong>Eingabe</strong>\n' +
+  '  oder die <strong>Leertaste</strong>, um das Element auszufÃ¼hren.</p>\n' +
+  '\n' +
+  '<h1>In Dialogfeldern ohne Registerkarten navigieren</h1>\n' +
+  '\n' +
+  '<p>In Dialogfeldern ohne Registerkarten ist beim Ãffnen eines Dialogfelds die erste interaktive Komponente fokussiert.</p>\n' +
+  '\n' +
+  '<p>Navigieren Sie zwischen den interaktiven Komponenten eines Dialogfelds, indem Sie <strong>TAB</strong> oder <strong>UMSCHALT+TAB</strong> drÃ¼cken.</p>\n' +
+  '\n' +
+  '<h1>In Dialogfeldern mit Registerkarten navigieren</h1>\n' +
+  '\n' +
+  '<p>In Dialogfeldern mit Registerkarten ist beim Ãffnen eines Dialogfelds die erste SchaltflÃ¤che eines RegisterkartenmenÃ¼s fokussiert.</p>\n' +
+  '\n' +
+  '<p>Navigieren Sie zwischen den interaktiven Komponenten auf dieser Registerkarte des Dialogfelds, indem Sie <strong>TAB</strong> oder\n' +
+  '  <strong>UMSCHALT+TAB</strong> drÃ¼cken.</p>\n' +
+  '\n' +
+  '<p>Wechseln Sie zu einer anderen Registerkarte des Dialogfelds, indem Sie den Fokus auf das RegisterkartenmenÃ¼ legen und dann die entsprechende <strong>Pfeiltaste</strong>\n' +
+  '  drÃ¼cken, um durch die verfÃ¼gbaren Registerkarten zu rotieren.</p>\n');
