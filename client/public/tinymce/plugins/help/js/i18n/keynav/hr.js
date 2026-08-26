ef3c38e29c0204c7a397dacdb99af1c103819d30
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:56:03 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/hr.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.hr',
+'<h1>PoÄetak navigacije na tipkovnici</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>Fokusiranje trake izbornika</dt>\n' +
+  '  <dd>Windows ili Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>Fokusiranje alatne trake</dt>\n' +
+  '  <dd>Windows ili Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>Fokusiranje podnoÅ¾ja</dt>\n' +
+  '  <dd>Windows ili Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>Fokusiranje obavijesti</dt>\n' +
+  '  <dd>Windows ili Linux: Alt + F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>Fokusiranje kontekstne alatne trake</dt>\n' +
+  '  <dd>Windows, Linux ili macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>Navigacija Äe zapoÄeti kod prve stavke na korisniÄkom suÄelju, koja Äe biti istaknuta ili podcrtana ako se radi o prvoj stavci u\n' +
+  '  putu elementa u podnoÅ¾ju.</p>\n' +
+  '\n' +
+  '<h1>Navigacija izmeÄu dijelova korisniÄkog suÄelja</h1>\n' +
+  '\n' +
+  '<p>Za pomicanje s jednog dijela korisniÄkog suÄelja na drugi pritisnite <strong>tabulator</strong>.</p>\n' +
+  '\n' +
+  '<p>Za pomicanje s jednog dijela korisniÄkog suÄelja na prethodni pritisnite <strong>Shift + tabulator</strong>.</p>\n' +
+  '\n' +
+  '<p>Ovo je redoslijed pomicanja <strong>tabulatora</strong> po dijelovima korisniÄkog suÄelja:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Traka izbornika</li>\n' +
+  '  <li>PojedinaÄne grupe na alatnoj traci</li>\n' +
+  '  <li>BoÄna traka</li>\n' +
+  '  <li>Put elemenata u podnoÅ¾ju</li>\n' +
+  '  <li>Gumb za pomicanje po broju rijeÄi u podnoÅ¾ju</li>\n' +
+  '  <li>Veza na brand u podnoÅ¾ju</li>\n' +
+  '  <li>ZnaÄajka za promjenu veliÄine alata za ureÄivanje u podnoÅ¾ju</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Ako neki dio korisniÄkog suÄelja nije naveden, on se preskaÄe.</p>\n' +
+  '\n' +
+  '<p>Ako u podnoÅ¾ju postoji fokus za navigaciju na tipkovnici, a nema vidljive boÄne trake, pritiskom na <strong>Shift + tabulator</strong>\n' +
+  '  fokus se prebacuje na prvu skupinu na alatnoj traci, ne na zadnju.</p>\n' +
+  '\n' +
+  '<h1>Navigacija unutar dijelova korisniÄkog suÄelja</h1>\n' +
+  '\n' +
+  '<p>Za pomicanje s jednog elementa korisniÄkog suÄelja na drugi pritisnite tipku s odgovarajuÄom <strong>strelicom</strong>.</p>\n' +
+  '\n' +
+  '<p>Tipke s <strong>lijevom</strong> i <strong>desnom</strong> strelicom</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>sluÅ¾e za pomicanje izmeÄu izbornika na alatnoj traci.</li>\n' +
+  '  <li>otvaraju podizbornik unutar izbornika.</li>\n' +
+  '  <li>sluÅ¾e za pomicanje izmeÄu gumba unutar skupina na alatnoj traci.</li>\n' +
+  '  <li>sluÅ¾e za pomicanje izmeÄu stavki na elementu puta u podnoÅ¾ju.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>Tipke s <strong>donjom</strong> i <strong>gornjom</strong> strelicom</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>sluÅ¾e za pomicanje izmeÄu stavki unutar izbornika.</li>\n' +
+  '  <li>sluÅ¾e za pomicanje izmeÄu stavki na alatnoj traci skoÄnog izbornika.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>Tipkama <strong>strelica</strong> kruÅ¾no se pomiÄete unutar dijela korisniÄkog suÄelja koji je u fokusu.</p>\n' +
+  '\n' +
+  '<p>Za zatvaranje otvorenog izbornika, otvorenog podizbornika ili otvorenog skoÄnog izbornika pritisnite tipku <strong>Esc</strong>.</p>\n' +
+  '\n' +
+  '<p>Ako je fokus trenutaÄno postavljen na vrh pojedinaÄnog dijela korisniÄkog suÄelja, pritiskom na tipku <strong>Esc</strong> takoÄer\n' +
+  '  u potpunosti zatvarate navigaciju na tipkovnici.</p>\n' +
+  '\n' +
+  '<h1>IzvrÅ¡avanje radnji putem stavki izbornika ili gumba na alatnoj traci</h1>\n' +
+  '\n' +
+  '<p>Nakon Å¡to se istakne stavka izbornika ili gumb na alatnoj traci s radnjom koju Å¾elite izvrÅ¡iti, pritisnite tipku <strong>Return</strong>, <strong>Enter</strong>\n' +
+  '  ili <strong>razmak</strong> da biste pokrenuli Å¾eljenu radnju.</p>\n' +
+  '\n' +
+  '<h1>Navigacija dijaloÅ¡kim okvirima izvan kartica</h1>\n' +
+  '\n' +
+  '<p>Prilikom otvaranja dijaloÅ¡kih okvira izvan kartica fokus se nalazi na prvoj interaktivnoj komponenti.</p>\n' +
+  '\n' +
+  '<p>Navigaciju izmeÄu interaktivnih dijaloÅ¡kih komponenata vrÅ¡ite pritiskom na <strong>tabulator</strong> ili <strong>Shift + tabulator</strong>.</p>\n' +
+  '\n' +
+  '<h1>Navigacija dijaloÅ¡kim okvirima u karticama</h1>\n' +
+  '\n' +
+  '<p>Prilikom otvaranja dijaloÅ¡kih okvira u karticama fokus se nalazi na prvom gumbu u izborniku unutar kartice.</p>\n' +
+  '\n' +
+  '<p>Navigaciju izmeÄu interaktivnih komponenata dijaloÅ¡kog okvira u kartici vrÅ¡ite pritiskom na <strong>tabulator</strong> ili\n' +
+  '  <strong>Shift + tabulator</strong>.</p>\n' +
+  '\n' +
+  '<p>Na karticu s drugim dijaloÅ¡kim okvirom moÅ¾ete se prebaciti tako da stavite fokus na izbornik kartice pa pritisnete tipku s odgovarajuÄom <strong>strelicom</strong>\n' +
+  '  za kruÅ¾no pomicanje izmeÄu dostupnih kartica.</p>\n');
