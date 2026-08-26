93aaa66be631274d4f5e67868ca30742cfd5263a
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 13:03:35 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ simulate_migration.js
@@ -0,0 +1,12 @@
+console.log('ð Simulando conexÃ£o ao banco...');
+console.log('â 13 tabelas criadas!');
+console.log('');
+console.log('-- Verificar template jurÃ­dico');
+console.log('SELECT * FROM templates;');
+console.log('-- Deve mostrar: "PetiÃ§Ã£o Inicial" com variÃ¡veis ${cliente}, ${oab}');
+console.log('');
+console.log('â ConexÃ£o com tabela users verificada!');
+console.log('ð ConexÃ£o encerrada.');
+console.log('');
+console.log('Status final');
+console.log('SELECT \'â SISTEMA JURÃDICO 100% OPERACIONAL\' as status;');
