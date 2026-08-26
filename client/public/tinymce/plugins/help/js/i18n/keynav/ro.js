b6e654ca3718479d30ae277ee20363a8b7288abe
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:56:28 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/ro.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.ro',
+'<h1>ÃncepeÈi navigarea de la tastaturÄ</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>Focalizare pe bara de meniu</dt>\n' +
+  '  <dd>Windows sau Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>Focalizare pe bara de instrumente</dt>\n' +
+  '  <dd>Windows sau Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>Focalizare pe subsol</dt>\n' +
+  '  <dd>Windows sau Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>Focalizare pe notificare</dt>\n' +
+  '  <dd>Windows sau Linux: Alt+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>Focalizare pe o barÄ de instrumente contextualÄ</dt>\n' +
+  '  <dd>Windows, Linux sau macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>Navigarea va Ã®ncepe de la primul element al interfeÈei cu utilizatorul, care va fi evidenÈiat sau subliniat Ã®n cazul primului element din\n' +
+  '  calea elementului Subsol.</p>\n' +
+  '\n' +
+  '<h1>NavigaÈi Ã®ntre secÈiunile interfeÈei cu utilizatorul</h1>\n' +
+  '\n' +
+  '<p>Pentru a trece de la o secÈiune a interfeÈei cu utilizatorul la alta, apÄsaÈi <strong>Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Pentru a trece de la o secÈiune a interfeÈei cu utilizatorul la cea anterioarÄ, apÄsaÈi <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Ordinea cu <strong>Tab</strong> a acestor secÈiuni ale interfeÈei cu utilizatorul este urmÄtoarea:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Bara de meniu</li>\n' +
+  '  <li>Fiecare grup de bare de instrumente</li>\n' +
+  '  <li>Bara lateralÄ</li>\n' +
+  '  <li>Calea elementului Ã®n subsol</li>\n' +
+  '  <li>Buton de comutare a numÄrului de cuvinte Ã®n subsol</li>\n' +
+  '  <li>Link de branding Ã®n subsol</li>\n' +
+  '  <li>MÃ¢ner de redimensionare a editorului Ã®n subsol</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Ãn cazul Ã®n care o secÈiune a interfeÈei cu utilizatorul nu este prezentÄ, aceasta este omisÄ.</p>\n' +
+  '\n' +
+  '<p>Ãn cazul Ã®n care subsolul are focalizarea navigaÈiei asupra tastaturii Èi nu existÄ o barÄ lateralÄ vizibilÄ, apÄsarea butonului <strong>Shift+Tab</strong>\n' +
+  '  mutÄ focalizarea pe primul grup de bare de instrumente, nu pe ultimul.</p>\n' +
+  '\n' +
+  '<h1>NavigaÈi Ã®n secÈiunile interfeÈei cu utilizatorul</h1>\n' +
+  '\n' +
+  '<p>Pentru a trece de la un element de interfaÈÄ cu utilizatorul la urmÄtorul, apÄsaÈi tasta cu <strong>sÄgeata</strong> corespunzÄtoare.</p>\n' +
+  '\n' +
+  '<p>Tastele cu sÄgeÈi cÄtre <strong>stÃ¢nga</strong> Èi <strong>dreapta</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>navigheazÄ Ã®ntre meniurile din bara de meniuri.</li>\n' +
+  '  <li>deschid un sub-meniu dintr-un meniu.</li>\n' +
+  '  <li>navigheazÄ Ã®ntre butoanele dintr-un grup de bare de instrumente.</li>\n' +
+  '  <li>navigheazÄ Ã®ntre elementele din calea elementelor subsolului.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>Tastele cu sÄgeÈi Ã®n <strong>sus</strong> Èi Ã®n <strong>jos</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>navigheazÄ Ã®ntre elementele de meniu dintr-un meniu.</li>\n' +
+  '  <li>navigheazÄ Ã®ntre elementele unui meniu pop-up din bara de instrumente.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>Tastele cu <strong>sÄgeÈi</strong> navigheazÄ Ã®n cadrul secÈiunii interfeÈei cu utilizatorul asupra cÄreia se focalizeazÄ.</p>\n' +
+  '\n' +
+  '<p>Pentru a Ã®nchide un meniu deschis, un sub-meniu deschis sau un meniu pop-up deschis, apÄsaÈi tasta <strong>Esc</strong>.</p>\n' +
+  '\n' +
+  '<p>DacÄ focalizarea curentÄ este asupra âpÄrÈii superioareâ a unei anumite secÈiuni a interfeÈei cu utilizatorul, prin apÄsarea tastei <strong>Esc</strong> se iese, de asemenea,\n' +
+  '  Ã®n Ã®ntregime din navigarea de la tastaturÄ.</p>\n' +
+  '\n' +
+  '<h1>Executarea unui element de meniu sau a unui buton din bara de instrumente</h1>\n' +
+  '\n' +
+  '<p>Atunci cÃ¢nd elementul de meniu dorit sau butonul dorit din bara de instrumente este evidenÈiat, apÄsaÈi <strong>Return</strong>, <strong>Enter</strong>,\n' +
+  '  sau <strong>bara de spaÈiu</strong> pentru a executa elementul.</p>\n' +
+  '\n' +
+  '<h1>Navigarea de dialoguri fÄrÄ file</h1>\n' +
+  '\n' +
+  '<p>Ãn dialogurile fÄrÄ file, prima componentÄ interactivÄ beneficiazÄ de focalizare la deschiderea dialogului.</p>\n' +
+  '\n' +
+  '<p>NavigaÈi Ã®ntre componentele dialogului interactiv apÄsÃ¢nd <strong>Tab</strong> sau <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<h1>Navigarea de dialoguri cu file</h1>\n' +
+  '\n' +
+  '<p>Ãn dialogurile cu file, primul buton din meniul cu file beneficiazÄ de focalizare la deschiderea dialogului.</p>\n' +
+  '\n' +
+  '<p>NavigaÈi Ã®ntre componentele interactive ale acestei file de dialog apÄsÃ¢nd <strong>Tab</strong> sau\n' +
+  '  <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>TreceÈi la o altÄ filÄ de dialog focalizÃ¢nd asupra meniului cu file Èi apoi apÄsÃ¢nd <strong>sÄgeata</strong> corespunzÄtoare\n' +
+  '  pentru a parcurge filele disponibile.</p>\n');
