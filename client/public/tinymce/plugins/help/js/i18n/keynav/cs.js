d15344c11851845a215889d5f1a80b292f85cfca
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:55:44 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/cs.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.cs',
+'<h1>ZaÄÃ­nÃ¡me navigovat pomocÃ­ klÃ¡vesnice</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>PÅejÃ­t na ÅÃ¡dek nabÃ­dek</dt>\n' +
+  '  <dd>Windows nebo Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>PÅejÃ­t na panel nÃ¡strojÅ¯</dt>\n' +
+  '  <dd>Windows nebo Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>PÅejÃ­t na zÃ¡patÃ­</dt>\n' +
+  '  <dd>Windows nebo Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>PÅejÃ­t na oznÃ¡menÃ­</dt>\n' +
+  '  <dd>Windows nebo Linux: Alt+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>PÅejÃ­t na kontextovÃ½ panel nÃ¡strojÅ¯</dt>\n' +
+  '  <dd>Windows, Linux nebo macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>Navigace zaÄne uÂ prvnÃ­ poloÅ¾ky uÅ¾ivatelskÃ©ho rozhranÃ­, kterÃ¡ bude zvÃ½raznÄna nebo vÂ pÅÃ­padÄ prvnÃ­ poloÅ¾ky\n' +
+  '  cesty kÂ prvku zÃ¡patÃ­ podtrÅ¾ena.</p>\n' +
+  '\n' +
+  '<h1>Navigace mezi oddÃ­ly uÅ¾ivatelskÃ©ho rozhranÃ­</h1>\n' +
+  '\n' +
+  '<p>StisknutÃ­m klÃ¡vesy <strong>Tab</strong> se posunete zÂ jednoho oddÃ­lu uÅ¾ivatelskÃ©ho rozhranÃ­ na dalÅ¡Ã­.</p>\n' +
+  '\n' +
+  '<p>StisknutÃ­m klÃ¡ves <strong>Shift+Tab</strong> se posunete zÂ jednoho oddÃ­lu uÅ¾ivatelskÃ©ho rozhranÃ­ na pÅedchozÃ­.</p>\n' +
+  '\n' +
+  '<p>PoÅadÃ­ pÅepÃ­nÃ¡nÃ­ mezi oddÃ­ly uÅ¾ivatelskÃ©ho rozhranÃ­ pomocÃ­ klÃ¡vesy <strong>Tab</strong>:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>ÅÃ¡dek nabÃ­dek</li>\n' +
+  '  <li>KaÅ¾dÃ¡ skupina panelu nÃ¡strojÅ¯</li>\n' +
+  '  <li>BoÄnÃ­ panel</li>\n' +
+  '  <li>Cesta kÂ prvku vÂ zÃ¡patÃ­.</li>\n' +
+  '  <li>TlaÄÃ­tko pÅepÃ­naÄe poÄtu slov vÂ zÃ¡patÃ­</li>\n' +
+  '  <li>Odkaz na informace oÂ znaÄce vÂ zÃ¡patÃ­</li>\n' +
+  '  <li>Ãchyt pro zmÄnu velikosti editoru vÂ zÃ¡patÃ­</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Pokud nÄjakÃ½ oddÃ­l uÅ¾ivatelskÃ©ho rozhranÃ­ nenÃ­ pÅÃ­tomen, je pÅeskoÄen.</p>\n' +
+  '\n' +
+  '<p>Pokud je zÃ¡patÃ­ vybranÃ© pro navigaci pomocÃ­ klÃ¡vesnice aÂ nenÃ­ zobrazen Å¾Ã¡dnÃ½ boÄnÃ­ panel, stisknutÃ­m klÃ¡ves <strong>Shift+Tab</strong>\n' +
+  '  pÅejdete na prvnÃ­ skupinu panelu nÃ¡strojÅ¯, nikoli na poslednÃ­.</p>\n' +
+  '\n' +
+  '<h1>Navigace vÂ rÃ¡mci oddÃ­lÅ¯ uÅ¾ivatelskÃ©ho rozhranÃ­</h1>\n' +
+  '\n' +
+  '<p>Chcete-li se pÅesunout zÂ jednoho prvku uÅ¾ivatelskÃ©ho rozhranÃ­ na dalÅ¡Ã­, stisknÄte pÅÃ­sluÅ¡nou klÃ¡vesu sÂ <strong>Å¡ipkou</strong>.</p>\n' +
+  '\n' +
+  '<p>KlÃ¡vesy sÂ Å¡ipkou <strong>vlevo</strong> aÂ <strong>vpravo</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>umoÅ¾ÅujÃ­ pÅesun mezi nabÃ­dkami na ÅÃ¡dku nabÃ­dek;</li>\n' +
+  '  <li>otevÃ­rajÃ­ podnabÃ­dku nabÃ­dky;</li>\n' +
+  '  <li>umoÅ¾ÅujÃ­ pÅesun mezi tlaÄÃ­tky ve skupinÄ panelu nÃ¡strojÅ¯;</li>\n' +
+  '  <li>umoÅ¾ÅujÃ­ pÅesun mezi poloÅ¾kami cesty prvku vÂ zÃ¡patÃ­.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>KlÃ¡vesy se Å¡ipkou <strong>dolÅ¯</strong> aÂ <strong>nahoru</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>umoÅ¾ÅujÃ­ pÅesun mezi poloÅ¾kami nabÃ­dky;</li>\n' +
+  '  <li>umoÅ¾ÅujÃ­ pÅesun mezi poloÅ¾kami mÃ­stnÃ­ nabÃ­dky panelu nÃ¡strojÅ¯.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p><strong>Å ipky</strong> provÃ¡dÃ­ pÅepÃ­nÃ¡nÃ­ vÂ rÃ¡mci vybranÃ©ho oddÃ­lu uÅ¾ivatelskÃ©ho rozhranÃ­.</p>\n' +
+  '\n' +
+  '<p>Chcete-li zavÅÃ­t otevÅenou nabÃ­dku, podnabÃ­dku nebo mÃ­stnÃ­ nabÃ­dku, stisknÄte klÃ¡vesu <strong>Esc</strong>.</p>\n' +
+  '\n' +
+  '<p>Pokud je aktuÃ¡lnÄ vybrÃ¡na hornÃ­ ÄÃ¡st oddÃ­lu uÅ¾ivatelskÃ©ho rozhranÃ­, stisknutÃ­m klÃ¡vesy <strong>Esc</strong> zcela ukonÄÃ­te takÃ©\n' +
+  '  navigaci pomocÃ­ klÃ¡vesnice.</p>\n' +
+  '\n' +
+  '<h1>ProvedenÃ­ pÅÃ­kazu poloÅ¾ky nabÃ­dky nebo tlaÄÃ­tka panelu nÃ¡strojÅ¯</h1>\n' +
+  '\n' +
+  '<p>Pokud je zvÃ½raznÄna poÅ¾adovanÃ¡ poloÅ¾ka nabÃ­dky nebo tlaÄÃ­tko panelu nÃ¡strojÅ¯, stisknutÃ­m klÃ¡vesy <strong>Return</strong>, <strong>Enter</strong>\n' +
+  '  nebo <strong>mezernÃ­ku</strong> provedete pÅÃ­sluÅ¡nÃ½ pÅÃ­kaz.</p>\n' +
+  '\n' +
+  '<h1>Navigace vÂ dialogovÃ½ch oknech bez zÃ¡loÅ¾ek</h1>\n' +
+  '\n' +
+  '<p>PÅi otevÅenÃ­ dialogovÃ½ch oken bez zÃ¡loÅ¾ek pÅejdete na prvnÃ­ interaktivnÃ­ komponentu.</p>\n' +
+  '\n' +
+  '<p>PÅechÃ¡zet mezi interaktivnÃ­mi komponentami dialogovÃ©ho okna mÅ¯Å¾ete stisknutÃ­m klÃ¡vesy <strong>Tab</strong> nebo kombinace <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<h1>Navigace vÂ dialogovÃ½ch oknech se zÃ¡loÅ¾kami</h1>\n' +
+  '\n' +
+  '<p>PÅi otevÅenÃ­ dialogovÃ½ch oken se zÃ¡loÅ¾kami pÅejdete na prvnÃ­ tlaÄÃ­tko vÂ nabÃ­dce zÃ¡loÅ¾ek.</p>\n' +
+  '\n' +
+  '<p>PÅechÃ¡zet mezi interaktivnÃ­mi komponentami tÃ©to zÃ¡loÅ¾ky dialogovÃ©ho okna mÅ¯Å¾ete stisknutÃ­m klÃ¡vesy <strong>Tab</strong> nebo\n' +
+  '  kombinace <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Chcete-li pÅepnout na dalÅ¡Ã­ zÃ¡loÅ¾ku dialogovÃ©ho okna, pÅejdÄte na nabÃ­dku zÃ¡loÅ¾ek aÂ potÃ© mÅ¯Å¾ete stisknutÃ­m poÅ¾adovanÃ© <strong>Å¡ipky</strong>\n' +
+  '  pÅepÃ­nat mezi dostupnÃ½mi zÃ¡loÅ¾kami.</p>\n');
