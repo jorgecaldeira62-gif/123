f0119b1b015af8a8fee6370be0bb5e6a729dc2d4
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:55:51 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/es.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.es',
+'<h1>Iniciar la navegaciÃ³n con el teclado</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>Enfocar la barra de menÃºs</dt>\n' +
+  '  <dd>Windows o Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>Enfocar la barra de herramientas</dt>\n' +
+  '  <dd>Windows o Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>Enfocar el pie de pÃ¡gina</dt>\n' +
+  '  <dd>Windows o Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>Enfocar la notificaciÃ³n</dt>\n' +
+  '  <dd>Windows o Linux: Alt+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>Enfocar una barra de herramientas contextual</dt>\n' +
+  '  <dd>Windows, Linux o macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>La navegaciÃ³n comenzarÃ¡ por el primer elemento de la interfaz de usuario (IU), de tal manera que se resaltarÃ¡, o bien se subrayarÃ¡ si se trata del primer elemento de\n' +
+  '  la ruta de elemento del pie de pÃ¡gina.</p>\n' +
+  '\n' +
+  '<h1>Navegar entre las secciones de la IU</h1>\n' +
+  '\n' +
+  '<p>Para pasar de una secciÃ³n de la IU a la siguiente, pulse la tecla <strong>Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Para pasar de una secciÃ³n de la IU a la anterior, pulse <strong>MayÃºs+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>El orden de <strong>tabulaciÃ³n</strong> de estas secciones de la IU es:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Barra de menÃºs</li>\n' +
+  '  <li>Cada grupo de barra de herramientas</li>\n' +
+  '  <li>Barra lateral</li>\n' +
+  '  <li>Ruta del elemento en el pie de pÃ¡gina</li>\n' +
+  '  <li>BotÃ³n de alternancia de recuento de palabras en el pie de pÃ¡gina</li>\n' +
+  '  <li>Enlace de personalizaciÃ³n de marca en el pie de pÃ¡gina</li>\n' +
+  '  <li>Controlador de cambio de tamaÃ±o en el pie de pÃ¡gina</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Si una secciÃ³n de la IU no estÃ¡ presente, esta se omite.</p>\n' +
+  '\n' +
+  '<p>Si el pie de pÃ¡gina tiene un enfoque de navegaciÃ³n con el teclado y no hay ninguna barra lateral visible, al pulsar <strong>MayÃºs+Tab</strong>,\n' +
+  '  el enfoque se moverÃ¡ al primer grupo de barra de herramientas, en lugar de al Ãºltimo.</p>\n' +
+  '\n' +
+  '<h1>Navegar dentro de las secciones de la IU</h1>\n' +
+  '\n' +
+  '<p>Para pasar de un elemento de la IU al siguiente, pulse la tecla de <strong>flecha</strong> correspondiente.</p>\n' +
+  '\n' +
+  '<p>Las teclas de flecha <strong>izquierda</strong> y <strong>derecha</strong> permiten</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>desplazarse entre los menÃºs de la barra de menÃºs.</li>\n' +
+  '  <li>abrir el submenÃº de un menÃº.</li>\n' +
+  '  <li>desplazarse entre los botones de un grupo de barra de herramientas.</li>\n' +
+  '  <li>desplazarse entre los elementos de la ruta de elemento del pie de pÃ¡gina.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>Las teclas de flecha <strong>abajo</strong> y <strong>arriba</strong> permiten</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>desplazarse entre los elementos de menÃº de un menÃº.</li>\n' +
+  '  <li>desplazarse entre los elementos de un menÃº emergente de una barra de herramientas.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>Las teclas de <strong>flecha</strong> van cambiando dentro de la secciÃ³n de la IU enfocada.</p>\n' +
+  '\n' +
+  '<p>Para cerrar un menÃº, un submenÃº o un menÃº emergente que estÃ©n abiertos, pulse la tecla <strong>Esc</strong>.</p>\n' +
+  '\n' +
+  '<p>Si el enfoque actual se encuentra en la parte superior de una secciÃ³n de la IU determinada, al pulsar la tecla <strong>Esc</strong> saldrÃ¡\n' +
+  '  de la navegaciÃ³n con el teclado por completo.</p>\n' +
+  '\n' +
+  '<h1>Ejecutar un elemento de menÃº o un botÃ³n de barra de herramientas</h1>\n' +
+  '\n' +
+  '<p>Si el elemento de menÃº o el botÃ³n de barra de herramientas deseado estÃ¡ resaltado, pulse la tecla <strong>Retorno</strong> o <strong>Entrar</strong>,\n' +
+  '  o la <strong>barra espaciadora</strong> para ejecutar el elemento.</p>\n' +
+  '\n' +
+  '<h1>Navegar por cuadros de diÃ¡logo sin pestaÃ±as</h1>\n' +
+  '\n' +
+  '<p>En los cuadros de diÃ¡logo sin pestaÃ±as, el primer componente interactivo se enfoca al abrirse el cuadro de diÃ¡logo.</p>\n' +
+  '\n' +
+  '<p>Para navegar entre los componentes interactivos del cuadro de diÃ¡logo, pulse las teclas <strong>Tab</strong> o <strong>MayÃºs+Tab</strong>.</p>\n' +
+  '\n' +
+  '<h1>Navegar por cuadros de diÃ¡logo con pestaÃ±as</h1>\n' +
+  '\n' +
+  '<p>En los cuadros de diÃ¡logo con pestaÃ±as, el primer botÃ³n del menÃº de pestaÃ±a se enfoca al abrirse el cuadro de diÃ¡logo.</p>\n' +
+  '\n' +
+  '<p>Para navegar entre componentes interactivos de esta pestaÃ±a del cuadro de diÃ¡logo, pulse las teclas <strong>Tab</strong> o\n' +
+  '  <strong>MayÃºs+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Si desea cambiar a otra pestaÃ±a del cuadro de diÃ¡logo, enfoque el menÃº de pestaÃ±as y, a continuaciÃ³n, pulse la tecla de <strong>flecha</strong>\n' +
+  '  correspondiente para moverse por las pestaÃ±as disponibles.</p>\n');
