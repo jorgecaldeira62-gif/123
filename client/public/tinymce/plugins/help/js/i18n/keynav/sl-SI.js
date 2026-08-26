d65cd9b83a14df8605697bb3003817cf33b530c7
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:56:32 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/sl-SI.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.sl-SI',
+'<h1>ZaÄetek krmarjenja s tipkovnico</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>Fokus na menijsko vrstico</dt>\n' +
+  '  <dd>Windows ali Linux: Alt + F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>Fokus na orodno vrstico</dt>\n' +
+  '  <dd>Windows ali Linux: Alt + F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>Fokus na nogo</dt>\n' +
+  '  <dd>Windows ali Linux: Alt + F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>OznaÄitev obvestila</dt>\n' +
+  '  <dd>Windows ali Linux: Alt + F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>Fokus na kontekstualno orodno vrstico</dt>\n' +
+  '  <dd>Windows, Linux ali macOS: Ctrl + F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>Krmarjenje se bo zaÄelo s prvim elementom uporabniÅ¡kega vmesnika, ki bo izpostavljena ali podÄrtan, Äe gre za prvi element na\n' +
+  '  poti do elementa noge.</p>\n' +
+  '\n' +
+  '<h1>Krmarjenje med razdelki uporabniÅ¡kega vmesnika</h1>\n' +
+  '\n' +
+  '<p>Äe se Å¾elite pomakniti z enega dela uporabniÅ¡kega vmesnika na naslednjega, pritisnite <strong>tabulatorko</strong>.</p>\n' +
+  '\n' +
+  '<p>Äe se Å¾elite pomakniti z enega dela uporabniÅ¡kega vmesnika na prejÅ¡njega, pritisnite <strong>shift + tabulatorko</strong>.</p>\n' +
+  '\n' +
+  '<p>Zaporedje teh razdelkov uporabniÅ¡kega vmesnika, ko pritiskate <strong>tabulatorko</strong>, je:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Menijska vrstica</li>\n' +
+  '  <li>Posamezne skupine orodne vrstice</li>\n' +
+  '  <li>Stranska vrstica</li>\n' +
+  '  <li>Pod do elementa v nogi</li>\n' +
+  '  <li>Gumb za preklop Å¡tetja besed v nogi</li>\n' +
+  '  <li>Povezava do blagovne znamke v nogi</li>\n' +
+  '  <li>RoÄaj za spreminjanje velikosti urejevalnika v nogi</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Äe razdelek uporabniÅ¡kega vmesnika ni prisoten, je preskoÄen.</p>\n' +
+  '\n' +
+  '<p>Äe ima noga fokus za krmarjenje s tipkovnico in ni vidne stranske vrstice, s pritiskom na <strong>shift + tabulatorko</strong>\n' +
+  '  fokus premaknete na prvo skupino orodne vrstice, ne zadnjo.</p>\n' +
+  '\n' +
+  '<h1>Krmarjenje v razdelkih uporabniÅ¡kega vmesnika</h1>\n' +
+  '\n' +
+  '<p>Äe se Å¾elite premakniti z enega elementa uporabniÅ¡kega vmesnika na naslednjega, pritisnite ustrezno <strong>puÅ¡ÄiÄno</strong> tipko.</p>\n' +
+  '\n' +
+  '<p><strong>Leva</strong> in <strong>desna</strong> puÅ¡ÄiÄna tipka</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>omogoÄata premikanje med meniji v menijski vrstici.</li>\n' +
+  '  <li>odpreta podmeni v meniju.</li>\n' +
+  '  <li>omogoÄata premikanje med gumbi v skupini orodne vrstice.</li>\n' +
+  '  <li>omogoÄata premikanje med elementi na poti do elementov noge.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p><strong>Spodnja</strong> in <strong>zgornja</strong> puÅ¡ÄiÄna tipka</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>omogoÄata premikanje med elementi menija.</li>\n' +
+  '  <li>omogoÄata premikanje med elementi v pojavnem meniju orodne vrstice.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p><strong>PuÅ¡ÄiÄne</strong> tipke omogoÄajo kroÅ¾enje znotraj razdelka uporabniÅ¡kega vmesnika, na katerem je fokus.</p>\n' +
+  '\n' +
+  '<p>Äe Å¾elite zapreti odprt meni, podmeni ali pojavni meni, pritisnite tipko <strong>Esc</strong>.</p>\n' +
+  '\n' +
+  '<p>Äe je trenutni fokus na Â»vrhuÂ« doloÄenega razdelka uporabniÅ¡kega vmesnika, s pritiskom tipke <strong>Esc</strong> zaprete\n' +
+  '  tudi celotno krmarjenje s tipkovnico.</p>\n' +
+  '\n' +
+  '<h1>Izvajanje menijskega elementa ali gumba orodne vrstice</h1>\n' +
+  '\n' +
+  '<p>Ko je oznaÄen Å¾eleni menijski element ali orodja vrstica, pritisnite <strong>vraÄalko</strong>, <strong>Enter</strong>\n' +
+  '  ali <strong>preslednico</strong>, da izvedete element.</p>\n' +
+  '\n' +
+  '<h1>Krmarjenje po pogovornih oknih brez zavihkov</h1>\n' +
+  '\n' +
+  '<p>Ko odprete pogovorno okno brez zavihkov, ima fokus prva interaktivna komponenta.</p>\n' +
+  '\n' +
+  '<p>Med interaktivnimi komponentami pogovornega okna se premikate s pritiskom <strong>tabulatorke</strong> ali kombinacije tipke <strong>shift + tabulatorke</strong>.</p>\n' +
+  '\n' +
+  '<h1>Krmarjenje po pogovornih oknih z zavihki</h1>\n' +
+  '\n' +
+  '<p>Ko odprete pogovorno okno z zavihki, ima fokus prvi gumb v meniju zavihka.</p>\n' +
+  '\n' +
+  '<p>Med interaktivnimi komponentami tega zavihka pogovornega okna se premikate s pritiskom <strong>tabulatorke</strong> ali\n' +
+  '  kombinacije tipke <strong>shift + tabulatorke</strong>.</p>\n' +
+  '\n' +
+  '<p>Na drug zavihek pogovornega okna preklopite tako, da fokus prestavite na meni zavihka in nato pritisnete ustrezno <strong>puÅ¡ÄiÄno</strong>\n' +
+  '  tipko, da se pomaknete med razpoloÅ¾ljivimi zavihki.</p>\n');
