91a3fefd8f18f1e7c637da88b9d66fd12a1f7377
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:56:22 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/pl.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.pl',
+'<h1>PoczÄtek nawigacji przy uÅ¼yciu klawiatury</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>Ustaw fokus na pasek menu</dt>\n' +
+  '  <dd>Windows lub Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>Ustaw fokus na pasek narzÄdzi</dt>\n' +
+  '  <dd>Windows lub Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>Ustaw fokus na sekcjÄ Footer</dt>\n' +
+  '  <dd>Windows lub Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>Skup siÄ na powiadomieniu</dt>\n' +
+  '  <dd>Windows lub Linux: Alt+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>Ustaw fokus na kontekstowy pasek narzÄdzi</dt>\n' +
+  '  <dd>Windows, Linux lub macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>Nawigacja zostanie rozpoczÄta od pierwszego elementu interfejsu uÅ¼ytkownika, ktÃ³ry jest podÅwietlony lub â w przypadku pierwszego elementu\n' +
+  '  w ÅcieÅ¼ce elementÃ³w w sekcji Footer â podkreÅlony.</p>\n' +
+  '\n' +
+  '<h1>Nawigacja pomiÄdzy sekcjami interfejsu uÅ¼ytkownika</h1>\n' +
+  '\n' +
+  '<p>Aby przenieÅÄ siÄ z danej sekcji interfejsu uÅ¼ytkownika do nastÄpnej, naciÅnij <strong>Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Aby przenieÅÄ siÄ z danej sekcji interfejsu uÅ¼ytkownika do poprzedniej, naciÅnij <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>KolejnoÅÄ klawisza <strong>Tab</strong> w takich sekcjach interfejsu uÅ¼ytkownika jest nastÄpujÄca:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Pasek menu</li>\n' +
+  '  <li>KaÅ¼da grupa na pasku narzÄdzi</li>\n' +
+  '  <li>Pasek boczny</li>\n' +
+  '  <li>ÅcieÅ¼ka elementÃ³w w sekcji Footer</li>\n' +
+  '  <li>Przycisk przeÅÄczania liczby sÅÃ³w w sekcji Footer</li>\n' +
+  '  <li>ÅÄcze brandujÄce w sekcji Footer</li>\n' +
+  '  <li>Uchwyt zmiany rozmiaru edytora w sekcji Footer</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>JeÅ¼eli nie ma sekcji interfejsu uÅ¼ytkownika, jest to pomijane.</p>\n' +
+  '\n' +
+  '<p>JeÅ¼eli na sekcji Footer jest ustawiony fokus nawigacji przy uÅ¼yciu klawiatury i nie ma widocznego paska bocznego, naciÅniÄcie <strong>Shift+Tab</strong>\n' +
+  '  przenosi fokus na pierwszÄ grupÄ paska narzÄdzi, a nie na ostatniÄ.</p>\n' +
+  '\n' +
+  '<h1>Nawigacja wewnÄtrz sekcji interfejsu uÅ¼ytkownika</h1>\n' +
+  '\n' +
+  '<p>Aby przenieÅÄ siÄ z danego elementu interfejsu uÅ¼ytkownika do nastÄpnego, naciÅnij odpowiedni klawisz <strong>strzaÅki</strong>.</p>\n' +
+  '\n' +
+  '<p>Klawisze strzaÅek <strong>w prawo</strong> i <strong>w lewo</strong> sÅuÅ¼Ä do</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>przenoszenia siÄ pomiÄdzy menu na pasku menu,</li>\n' +
+  '  <li>otwarcia podmenu w menu,</li>\n' +
+  '  <li>przenoszenia siÄ pomiÄdzy przyciskami w grupie paska narzÄdzi,</li>\n' +
+  '  <li>przenoszenia siÄ pomiÄdzy elementami w ÅcieÅ¼ce elementÃ³w w sekcji Footer.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>Klawisze strzaÅek <strong>w dÃ³Å</strong> i <strong>w gÃ³rÄ</strong> sÅuÅ¼Ä do</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>przenoszenia siÄ pomiÄdzy elementami menu w menu,</li>\n' +
+  '  <li>przenoszenia siÄ pomiÄdzy elementami w wyskakujÄcym menu paska narzÄdzi.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>Klawisze <strong>strzaÅek</strong> sÅuÅ¼Ä do przemieszczania siÄ w sekcji interfejsu uÅ¼ytkownika z ustawionym fokusem.</p>\n' +
+  '\n' +
+  '<p>Aby zamknÄÄ otwarte menu, otwarte podmenu lub otwarte menu wyskakujÄce, naciÅnij klawisz <strong>Esc</strong>.</p>\n' +
+  '\n' +
+  '<p>JeÅ¼eli fokus jest ustawiony na gÃ³rze konkretnej sekcji interfejsu uÅ¼ytkownika, naciÅniÄcie klawisza <strong>Esc</strong> powoduje wyjÅcie\n' +
+  '  z nawigacji przy uÅ¼yciu klawiatury.</p>\n' +
+  '\n' +
+  '<h1>Wykonanie elementu menu lub przycisku paska narzÄdzi</h1>\n' +
+  '\n' +
+  '<p>Gdy podÅwietlony jest Å¼Ädany element menu lub przycisk paska narzÄdzi, naciÅnij klawisz <strong>Return</strong>, <strong>Enter</strong>\n' +
+  '  lub <strong>Spacja</strong>, aby go wykonaÄ.</p>\n' +
+  '\n' +
+  '<h1>Nawigacja po oknie dialogowym bez kart</h1>\n' +
+  '\n' +
+  '<p>Gdy otwiera siÄ okno dialogowe bez kart, fokus ustawiany jest na pierwszÄ interaktywnÄ czÄÅÄ okna.</p>\n' +
+  '\n' +
+  '<p>PomiÄdzy interaktywnymi czÄÅciami okna dialogowego nawiguj, naciskajÄc klawisze <strong>Tab</strong> lub <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<h1>Nawigacja po oknie dialogowym z kartami</h1>\n' +
+  '\n' +
+  '<p>W przypadku okna dialogowego z kartami po otwarciu okna dialogowego fokus ustawiany jest na pierwszy przycisk w menu karty.</p>\n' +
+  '\n' +
+  '<p>NawigacjÄ pomiÄdzy interaktywnymi czÄÅciami karty okna dialogowego prowadzi siÄ poprzez naciskanie klawiszy <strong>Tab</strong> lub\n' +
+  '  <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>PrzeÅÄczenie siÄ na innÄ kartÄ okna dialogowego wykonuje siÄ poprzez ustawienie fokusu na menu karty i naciÅniÄcie odpowiedniego klawisza <strong>strzaÅki</strong>\n' +
+  '  w celu przemieszczenia siÄ pomiÄdzy dostÄpnymi kartami.</p>\n');
