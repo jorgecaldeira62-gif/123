ede9361dd72a0d49231a7e40b0dbcb38e363d934
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:56:36 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/sv_SE.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.sv_SE',
+'<h1>PÃ¥bÃ¶rja tangentbordsnavigering</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>Fokusera pÃ¥ menyraden</dt>\n' +
+  '  <dd>Windows eller Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>Fokusera pÃ¥ verktygsraden</dt>\n' +
+  '  <dd>Windows eller Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>Fokusera pÃ¥ verktygsraden</dt>\n' +
+  '  <dd>Windows eller Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>Fokusera aviseringen</dt>\n' +
+  '  <dd>Windows eller Linux: Alt+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>Fokusera pÃ¥ en snabbverktygsrad</dt>\n' +
+  '  <dd>Windows, Linux eller macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>Navigeringen bÃ¶rjar vid det fÃ¶rsta grÃ¤nssnittsobjektet, vilket Ã¤r markerat eller understruket om det gÃ¤ller det fÃ¶rsta objektet i\n' +
+  '  sidfotens elementsÃ¶kvÃ¤g.</p>\n' +
+  '\n' +
+  '<h1>Navigera mellan UI-avsnitt</h1>\n' +
+  '\n' +
+  '<p>Flytta frÃ¥n ett UI-avsnitt till nÃ¤sta genom att trycka pÃ¥ <strong>Tabb</strong>.</p>\n' +
+  '\n' +
+  '<p>Flytta frÃ¥n ett UI-avsnitt till det fÃ¶regÃ¥ende genom att trycka pÃ¥ <strong>Skift+Tabb</strong>.</p>\n' +
+  '\n' +
+  '<p><strong>Tabb</strong>-ordningen fÃ¶r dessa UI-avsnitt Ã¤r:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Menyrad</li>\n' +
+  '  <li>Varje verktygsradsgrupp</li>\n' +
+  '  <li>Sidoruta</li>\n' +
+  '  <li>ElementsÃ¶kvÃ¤g i sidfoten</li>\n' +
+  '  <li>VÃ¤xlingsknapp fÃ¶r ordantal i sidfoten</li>\n' +
+  '  <li>VarumÃ¤rkeslÃ¤nk i sidfoten</li>\n' +
+  '  <li>Storlekshandtag fÃ¶r redigeraren i sidfoten</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Om ett UI-avsnitt inte finns hoppas det Ã¶ver.</p>\n' +
+  '\n' +
+  '<p>Om sidfoten har fokus pÃ¥ tangentbordsnavigering, och det inte finns nÃ¥gon synlig sidoruta, flyttas fokus till den fÃ¶rsta verktygsradsgruppen\n' +
+  '  nÃ¤r du trycker pÃ¥ <strong>Skift+Tabb</strong>, inte till den sista.</p>\n' +
+  '\n' +
+  '<h1>Navigera i UI-avsnitt</h1>\n' +
+  '\n' +
+  '<p>Flytta frÃ¥n ett UI-element till nÃ¤sta genom att trycka pÃ¥ motsvarande <strong>piltangent</strong>.</p>\n' +
+  '\n' +
+  '<p><strong>VÃ¤nsterpil</strong> och <strong>hÃ¶gerpil</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>flytta mellan menyer pÃ¥ menyraden.</li>\n' +
+  '  <li>Ã¶ppna en undermeny pÃ¥ en meny.</li>\n' +
+  '  <li>flytta mellan knappar i en verktygsradgrupp.</li>\n' +
+  '  <li>flytta mellan objekt i sidfotens elementsÃ¶kvÃ¤g.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p><strong>Nedpil</strong> och <strong>uppil</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>flytta mellan menyalternativ pÃ¥ en meny.</li>\n' +
+  '  <li>flytta mellan alternativ pÃ¥ en popup-meny pÃ¥ verktygsraden.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p><strong>Piltangenterna</strong> cirkulerar inom det fokuserade UI-avsnittet.</p>\n' +
+  '\n' +
+  '<p>Tryck pÃ¥ <strong>Esc</strong>-tangenten om du vill stÃ¤nga en Ã¶ppen meny, undermeny eller popup-meny.</p>\n' +
+  '\n' +
+  '<p>Om det aktuella fokuset Ã¤r hÃ¶gst upp i ett UI-avsnitt avlutas Ã¤ven tangentbordsnavigeringen helt nÃ¤r\n' +
+  '  du trycker pÃ¥ <strong>Esc</strong>-tangenten.</p>\n' +
+  '\n' +
+  '<h1>KÃ¶ra ett menyalternativ eller en verktygfÃ¤ltsknapp</h1>\n' +
+  '\n' +
+  '<p>NÃ¤r menyalternativet eller verktygsradsknappen Ã¤r markerad trycker du pÃ¥ <strong>Retur</strong>, <strong>Enter</strong>\n' +
+  '  eller <strong>blanksteg</strong> fÃ¶r att kÃ¶ra alternativet.</p>\n' +
+  '\n' +
+  '<h1>Navigera i dialogrutor utan flikar</h1>\n' +
+  '\n' +
+  '<p>I dialogrutor utan flikar Ã¤r den fÃ¶rsta interaktiva komponenten i fokus nÃ¤r dialogrutan Ã¶ppnas.</p>\n' +
+  '\n' +
+  '<p>Navigera mellan interaktiva dialogkomponenter genom att trycka pÃ¥ <strong>Tabb</strong> eller <strong>Skift+Tabb</strong>.</p>\n' +
+  '\n' +
+  '<h1>Navigera i dialogrutor med flikar</h1>\n' +
+  '\n' +
+  '<p>I dialogrutor utan flikar Ã¤r den fÃ¶rsta knappen pÃ¥ flikmenyn i fokus nÃ¤r dialogrutan Ã¶ppnas.</p>\n' +
+  '\n' +
+  '<p>Navigera mellan interaktiva komponenter pÃ¥ dialogrutefliken genom att trycka pÃ¥ <strong>Tabb</strong> eller\n' +
+  '  <strong>Skift+Tabb</strong>.</p>\n' +
+  '\n' +
+  '<p>VÃ¤xla till en annan dialogruta genom att fokusera pÃ¥ flikmenyn och sedan trycka pÃ¥ motsvarande <strong>piltangent</strong>\n' +
+  '  fÃ¶r att cirkulera mellan de tillgÃ¤ngliga flikarna.</p>\n');
