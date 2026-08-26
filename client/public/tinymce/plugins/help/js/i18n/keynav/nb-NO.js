bf2c1f50187376635f3b62eede29c4a573e8e662
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:56:17 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/nb-NO.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.nb-NO',
+'<h1>Starte tastaturnavigering</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>Utheve menylinjen</dt>\n' +
+  '  <dd>Windows eller Linux: AltÂ +Â F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>Utheve verktÃ¸ylinjen</dt>\n' +
+  '  <dd>Windows eller Linux: AltÂ +Â F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>Utheve bunnteksten</dt>\n' +
+  '  <dd>Windows eller Linux: AltÂ +Â F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>Fokuser pÃ¥ varselet</dt>\n' +
+  '  <dd>Windows eller Linux: Alt+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>Utheve en kontekstuell verktÃ¸ylinje</dt>\n' +
+  '  <dd>Windows, Linux eller macOS: CtrlÂ +Â F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>Navigeringen starter ved det fÃ¸rste grensesnittelementet, som utheves, eller understrekes nÃ¥r det gjelder det fÃ¸rste elementet i\n' +
+  '  elementstien i bunnteksten.</p>\n' +
+  '\n' +
+  '<h1>Navigere mellom grensesnittdeler</h1>\n' +
+  '\n' +
+  '<p>Du kan bevege deg fra Ã©n grensesnittdel til den neste ved Ã¥ trykke pÃ¥ <strong>tabulatortasten</strong>.</p>\n' +
+  '\n' +
+  '<p>Du kan bevege deg fra Ã©n grensesnittdel til den forrige ved Ã¥ trykke pÃ¥ <strong>Shift + tabulatortasten</strong>.</p>\n' +
+  '\n' +
+  '<p>RekkefÃ¸lgen til <strong>tabulatortasten</strong> gjennom grensesnittdelene er:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Menylinjen</li>\n' +
+  '  <li>Hver gruppe pÃ¥ verktÃ¸ylinjen</li>\n' +
+  '  <li>Sidestolpen</li>\n' +
+  '  <li>Elementstien i bunnteksten</li>\n' +
+  '  <li>Veksleknappen for ordantall i bunnteksten</li>\n' +
+  '  <li>Merkelenken i bunnteksten</li>\n' +
+  '  <li>SkaleringshÃ¥ndtaket for redigeringsprogrammet i bunnteksten</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Hvis en grensesnittdel ikke er til stede, blir den hoppet over.</p>\n' +
+  '\n' +
+  '<p>Hvis tastaturnavigeringen har uthevet bunnteksten og det ikke finnes en synlig sidestolpe, kan du trykke pÃ¥ <strong>Shift + tabulatortasten</strong>\n' +
+  '  for Ã¥ flytte fokuset til den fÃ¸rste gruppen pÃ¥ verktÃ¸ylinjen i stedet for den siste.</p>\n' +
+  '\n' +
+  '<h1>Navigere innenfor grensesnittdeler</h1>\n' +
+  '\n' +
+  '<p>Du kan bevege deg fra ett grensesnittelement til det neste ved Ã¥ trykke pÃ¥ den aktuelle <strong>piltasten</strong>.</p>\n' +
+  '\n' +
+  '<p>De <strong>venstre</strong> og <strong>hÃ¸yre</strong> piltastene</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>beveger deg mellom menyer pÃ¥ menylinjen.</li>\n' +
+  '  <li>Ã¥pner en undermeny i en meny.</li>\n' +
+  '  <li>beveger deg mellom knapper i en gruppe pÃ¥ verktÃ¸ylinjen.</li>\n' +
+  '  <li>beveger deg mellom elementer i elementstien i bunnteksten.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p><strong>Ned</strong>- og <strong>opp</strong>-piltastene</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>beveger deg mellom menyelementer i en meny.</li>\n' +
+  '  <li>beveger deg mellom elementer i en hurtigmeny pÃ¥ verktÃ¸ylinjen.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>Med <strong>piltastene</strong> kan du bevege deg innenfor den uthevede grensesnittdelen.</p>\n' +
+  '\n' +
+  '<p>Du kan lukke en Ã¥pen meny, en Ã¥pen undermeny eller en Ã¥pen hurtigmeny ved Ã¥ klikke pÃ¥ <strong>Esc</strong>-tasten.</p>\n' +
+  '\n' +
+  '<p>Hvis det Ã¸verste nivÃ¥et i en grensesnittdel er uthevet, kan du ved Ã¥ trykke pÃ¥ <strong>Esc</strong> ogsÃ¥ avslutte\n' +
+  '  tastaturnavigeringen helt.</p>\n' +
+  '\n' +
+  '<h1>UtfÃ¸re et menyelement eller en knapp pÃ¥ en verktÃ¸ylinje</h1>\n' +
+  '\n' +
+  '<p>NÃ¥r det Ã¸nskede menyelementet eller verktÃ¸ylinjeknappen er uthevet, trykker du pÃ¥ <strong>Retur</strong>, <strong>Enter</strong>,\n' +
+  '  eller <strong>mellomromstasten</strong> for Ã¥ utfÃ¸re elementet.</p>\n' +
+  '\n' +
+  '<h1>Navigere i dialogbokser uten faner</h1>\n' +
+  '\n' +
+  '<p>I dialogbokser uten faner blir den fÃ¸rste interaktive komponenten uthevet nÃ¥r dialogboksen Ã¥pnes.</p>\n' +
+  '\n' +
+  '<p>Naviger mellom interaktive komponenter i dialogboksen ved Ã¥ trykke pÃ¥ <strong>tabulatortasten</strong> eller <strong>Shift + tabulatortasten</strong>.</p>\n' +
+  '\n' +
+  '<h1>Navigere i fanebaserte dialogbokser</h1>\n' +
+  '\n' +
+  '<p>I fanebaserte dialogbokser blir den fÃ¸rste knappen i fanemenyen uthevet nÃ¥r dialogboksen Ã¥pnes.</p>\n' +
+  '\n' +
+  '<p>Naviger mellom interaktive komponenter i fanen ved Ã¥ trykke pÃ¥ <strong>tabulatortasten</strong> eller\n' +
+  '  <strong>Shift + tabulatortasten</strong>.</p>\n' +
+  '\n' +
+  '<p>Veksle til en annen fane i dialogboksen ved Ã¥ utheve fanemenyen, og trykk deretter pÃ¥ den aktuelle <strong>piltasten</strong>\n' +
+  '  for Ã¥ bevege deg mellom de tilgjengelige fanene.</p>\n');
