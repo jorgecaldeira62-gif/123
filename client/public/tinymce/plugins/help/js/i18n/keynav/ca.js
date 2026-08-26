6d0000b933afe4de14107fd222823bbea757ea97
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:55:42 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/ca.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.ca',
+'<h1>Inici de la navegaciÃ³ amb el teclat</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>Enfocar la barra de menÃºs</dt>\n' +
+  '  <dd>Windows o Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  "  <dt>Enfocar la barra d'eines</dt>\n" +
+  '  <dd>Windows o Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>Enfocar el peu de pÃ gina</dt>\n' +
+  '  <dd>Windows o Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>Enfocar la notificaciÃ³</dt>\n' +
+  '  <dd>Windows o Linux: Alt+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  "  <dt>Enfocar una barra d'eines contextual</dt>\n" +
+  '  <dd>Windows, Linux o macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  "<p>La navegaciÃ³ comenÃ§arÃ  en el primer element de la interfÃ­cie d'usuari, que es ressaltarÃ  o subratllarÃ  per al primer element a\n" +
+  "  la ruta de l'element de peu de pÃ gina.</p>\n" +
+  '\n' +
+  "<h1>NavegaciÃ³ entre seccions de la interfÃ­cie d'usuari</h1>\n" +
+  '\n' +
+  "<p>Per desplaÃ§ar-vos des d'una secciÃ³ de la interfÃ­cie d'usuari a la segÃ¼ent, premeu la tecla <strong>Tab</strong>.</p>\n" +
+  '\n' +
+  "<p>Per desplaÃ§ar-vos des d'una secciÃ³ de la interfÃ­cie d'usuari a l'anterior, premeu les tecles <strong>Maj+Tab</strong>.</p>\n" +
+  '\n' +
+  "<p>L'ordre en prÃ©mer la tecla <strong>Tab</strong> d'aquestes secciones de la interfÃ­cie d'usuari Ã©s:</p>\n" +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Barra de menÃºs</li>\n' +
+  "  <li>Cada grup de la barra d'eines</li>\n" +
+  '  <li>Barra lateral</li>\n' +
+  "  <li>Ruta de l'element del peu de pÃ gina</li>\n" +
+  '  <li>BotÃ³ de commutaciÃ³ de recompte de paraules al peu de pÃ gina</li>\n' +
+  '  <li>EnllaÃ§ de marca del peu de pÃ gina</li>\n' +
+  "  <li>Control de canvi de mida de l'editor al peu de pÃ gina</li>\n" +
+  '</ol>\n' +
+  '\n' +
+  "<p>Si no hi ha una secciÃ³ de la interfÃ­cie d'usuari, s'ometrÃ .</p>\n" +
+  '\n' +
+  '<p>Si el peu de pÃ gina tÃ© el focus de navegaciÃ³ del teclat i no hi ha cap barra lateral visible, en prÃ©mer <strong>Maj+Tab</strong>\n' +
+  "  el focus es mou al primer grup de la barra d'eines, no l'Ãºltim.</p>\n" +
+  '\n' +
+  "<h1>NavegaciÃ³ dins de les seccions de la interfÃ­cie d'usuari</h1>\n" +
+  '\n' +
+  "<p>Per desplaÃ§ar-vos des d'un element de la interfÃ­cie d'usuari al segÃ¼ent, premeu la tecla de <strong>Fletxa</strong> adequada.</p>\n" +
+  '\n' +
+  '<p>Les tecles de fletxa <strong>Esquerra</strong> i <strong>Dreta</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>us permeten desplaÃ§ar-vos entre menÃºs de la barra de menÃºs.</li>\n' +
+  '  <li>obren un submenÃº en un menÃº.</li>\n' +
+  "  <li>us permeten desplaÃ§ar-vos entre botons d'un grup de la barra d'eines.</li>\n" +
+  "  <li>us permeten desplaÃ§ar-vos entre elements de la ruta d'elements del peu de pÃ gina.</li>\n" +
+  '</ul>\n' +
+  '\n' +
+  '<p>Les tecles de fletxa <strong>Avall</strong> i <strong>Amunt</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  "  <li>us permeten desplaÃ§ar-vos entre elements de menÃº d'un menÃº.</li>\n" +
+  "  <li>us permeten desplaÃ§ar-vos entre elements d'un menÃº emergent de la barra d'eines.</li>\n" +
+  '</ul>\n' +
+  '\n' +
+  "<p>Les tecles de <strong>Fletxa</strong> us permeten desplaÃ§ar-vos dins de la secciÃ³ de la interfÃ­cie d'usuari que tÃ© el focus.</p>\n" +
+  '\n' +
+  '<p>Per tancar un menÃº, un submenÃº o un menÃº emergent oberts, premeu la tecla <strong>Esc</strong>.</p>\n' +
+  '\n' +
+  "<p>Si el focus actual es troba a la âpart superiorâ d'una secciÃ³ especÃ­fica de la interfÃ­cie d'usuari, en prÃ©mer la tecla <strong>Esc</strong> tambÃ© es tanca\n" +
+  '  completament la navegaciÃ³ amb el teclat.</p>\n' +
+  '\n' +
+  "<h1>ExecuciÃ³ d'un element de menÃº o d'un botÃ³ de la barra d'eines</h1>\n" +
+  '\n' +
+  "<p>Quan l'element del menÃº o el botÃ³ de la barra d'eines que desitgeu estigui ressaltat, premeu <strong>Retorn</strong>, <strong>Intro</strong>\n" +
+  "  o la <strong>barra d'espai</strong> per executar l'element.</p>\n" +
+  '\n' +
+  '<h1>NavegaciÃ³ per quadres de diÃ leg sense pestanyes</h1>\n' +
+  '\n' +
+  "<p>En els quadres de diÃ leg sense pestanyes, el primer component interactiu pren el focus quan s'obre el quadre diÃ leg.</p>\n" +
+  '\n' +
+  '<p>Premeu la tecla <strong>Tab</strong> o les tecles <strong>Maj+Tab</strong> per desplaÃ§ar-vos entre components interactius del quadre de diÃ leg.</p>\n' +
+  '\n' +
+  '<h1>NavegaciÃ³ per quadres de diÃ leg amb pestanyes</h1>\n' +
+  '\n' +
+  "<p>En els quadres de diÃ leg amb pestanyes, el primer botÃ³ del menÃº de la pestanya pren el focus quan s'obre el quadre diÃ leg.</p>\n" +
+  '\n' +
+  "<p>Per desplaÃ§ar-vos entre components interactius d'aquest quadre de diÃ leg, premeu la tecla <strong>Tab</strong> o\n" +
+  '  les tecles <strong>Maj+Tab</strong>.</p>\n' +
+  '\n' +
+  "<p>Canvieu a la pestanya d'un altre quadre de diÃ leg, tot enfocant el menÃº de la pestanya, i desprÃ©s premeu la tecla <strong>Fletxa</strong> adequada\n" +
+  '  per canviar entre les pestanyes disponibles.</p>\n');
