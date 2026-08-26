1c66d72dfb6b3210d090b8ff8e398854f14663b6
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:56:31 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/sk.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.sk',
+'<h1>ZaÄÃ­name sÂ navigÃ¡ciou pomocou klÃ¡vesnice</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>PrejsÅ¥ na panel sÂ ponukami</dt>\n' +
+  '  <dd>Windows alebo Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>PrejsÅ¥ na panel nÃ¡strojov</dt>\n' +
+  '  <dd>Windows alebo Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>PrejsÅ¥ na pÃ¤tiÄku</dt>\n' +
+  '  <dd>Windows alebo Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>ZaostriÅ¥ na oznÃ¡menie</dt>\n' +
+  '  <dd>Windows alebo Linux: Alt+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>PrejsÅ¥ na kontextovÃ½ panel nÃ¡strojov</dt>\n' +
+  '  <dd>Windows, Linux alebo macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>NavigÃ¡cia zaÄne pri prvej poloÅ¾ke pouÅ¾Ã­vateÄ¾skÃ©ho rozhrania, ktorÃ¡ bude zvÃ½raznenÃ¡ alebo vÂ prÃ­pade prvej poloÅ¾ky\n' +
+  '  cesty kÂ pÃ¤tiÄke podÄiarknutÃ¡.</p>\n' +
+  '\n' +
+  '<h1>NavigÃ¡cia medzi ÄasÅ¥ami pouÅ¾Ã­vateÄ¾skÃ©ho rozhrania</h1>\n' +
+  '\n' +
+  '<p>Ak sa chcete posunÃºÅ¥ zÂ jednej Äasti pouÅ¾Ã­vateÄ¾skÃ©ho rozhrania do druhej, stlaÄte tlaÄidlo <strong>Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Ak sa chcete posunÃºÅ¥ zÂ jednej Äasti pouÅ¾Ã­vateÄ¾skÃ©ho rozhrania do predchÃ¡dzajÃºcej, stlaÄte tlaÄidlÃ¡ <strong>Shift + Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Poradie prepÃ­nania medzi tÃ½mito ÄasÅ¥ami pouÅ¾Ã­vateÄ¾skÃ©ho rozhrania pri stlÃ¡ÄanÃ­ tlaÄidla <strong>Tab</strong>:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Panel sÂ ponukou</li>\n' +
+  '  <li>KaÅ¾dÃ¡ skupina panela nÃ¡strojov</li>\n' +
+  '  <li>BoÄnÃ½ panel</li>\n' +
+  '  <li>Cesta kÂ prvku vÂ pÃ¤tiÄke</li>\n' +
+  '  <li>PrepÃ­naÄ poÄtu slov vÂ pÃ¤tiÄke</li>\n' +
+  '  <li>Odkaz na informÃ¡cie oÂ znaÄke vÂ pÃ¤tiÄke</li>\n' +
+  '  <li>Ãchyt na zmenu veÄ¾kosti editora vÂ pÃ¤tiÄke</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Ak nejakÃ¡ ÄasÅ¥ pouÅ¾Ã­vateÄ¾skÃ©ho rozhrania nie je prÃ­tomnÃ¡, preskoÄÃ­ sa.</p>\n' +
+  '\n' +
+  '<p>Ak je pÃ¤tiÄka vybratÃ¡ na navigÃ¡ciu pomocou klÃ¡vesnice a nie je viditeÄ¾nÃ½ boÄnÃ½ panel, stlaÄenÃ­m klÃ¡vesov <strong>Shift+Tab</strong>\n' +
+  '  prejdete na prvÃº skupinu panela nÃ¡strojov, nie na poslednÃº.</p>\n' +
+  '\n' +
+  '<h1>NavigÃ¡cia vÂ rÃ¡mci ÄastÃ­ pouÅ¾Ã­vateÄ¾skÃ©ho rozhrania</h1>\n' +
+  '\n' +
+  '<p>Ak sa chcete posunÃºÅ¥ zÂ jednÃ©ho prvku pouÅ¾Ã­vateÄ¾skÃ©ho rozhrania na ÄalÅ¡Ã­, stlaÄte prÃ­sluÅ¡nÃ½ klÃ¡ves so <strong>Å¡Ã­pkou</strong>.</p>\n' +
+  '\n' +
+  '<p>KlÃ¡vesy so Å¡Ã­pkami <strong>doÄ¾ava</strong> a <strong>doprava</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>umoÅ¾ÅujÃº presun medzi ponukami na paneli ponÃºk,</li>\n' +
+  '  <li>otvÃ¡rajÃº podponuku vÂ rÃ¡mci ponuky,</li>\n' +
+  '  <li>umoÅ¾ÅujÃº presun medzi tlaÄidlami vÂ skupine panelov nÃ¡strojov,</li>\n' +
+  '  <li>umoÅ¾ÅujÃº presun medzi poloÅ¾kami cesty prvku vÂ pÃ¤tiÄke.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>KlÃ¡vesy so Å¡Ã­pkami <strong>dole</strong> a <strong>hore</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>umoÅ¾ÅujÃº presun medzi poloÅ¾kami ponuky,</li>\n' +
+  '  <li>umoÅ¾ÅujÃº presun medzi poloÅ¾kami vÂ kontextovej ponuke panela nÃ¡strojov.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>KlÃ¡vesy so <strong>Å¡Ã­pkami</strong> vykonÃ¡vajÃº prepÃ­nanie vÂ rÃ¡mci vybranej Äasti pouÅ¾Ã­vateÄ¾skÃ©ho rozhrania.</p>\n' +
+  '\n' +
+  '<p>Ak chcete zatvoriÅ¥ otvorenÃº ponuku, otvorenÃº podponuku alebo otvorenÃº kontextovÃº ponuku, stlaÄte klÃ¡ves <strong>Esc</strong>.</p>\n' +
+  '\n' +
+  '<p>Ak je aktuÃ¡lne vybratÃ¡ hornÃ¡ ÄasÅ¥ konkrÃ©tneho pouÅ¾Ã­vateÄ¾skÃ©ho rozhrania, stlaÄenÃ­m klÃ¡vesu <strong>Esc</strong> Ãºplne ukonÄÃ­te tieÅ¾\n' +
+  '  navigÃ¡ciu pomocou klÃ¡vesnice.</p>\n' +
+  '\n' +
+  '<h1>Vykonanie prÃ­kazu poloÅ¾ky ponuky alebo tlaÄidla panela nÃ¡strojov</h1>\n' +
+  '\n' +
+  '<p>KeÄ je zvÃ½raznenÃ¡ poÅ¾adovanÃ¡ poloÅ¾ka ponuky alebo tlaÄidlo panela nÃ¡strojov, stlaÄenÃ­m klÃ¡vesov <strong>Return</strong>, <strong>Enter</strong>\n' +
+  '  alebo <strong>medzernÃ­ka</strong> vykonÃ¡te prÃ­sluÅ¡nÃ½ prÃ­kaz poloÅ¾ky.</p>\n' +
+  '\n' +
+  '<h1>NavigÃ¡cia vÂ dialÃ³govÃ½ch oknÃ¡ch bez zÃ¡loÅ¾iek</h1>\n' +
+  '\n' +
+  '<p>Pri otvorenÃ­ dialÃ³govÃ½ch okien bez zÃ¡loÅ¾iek prejdete na prvÃ½ interaktÃ­vny komponent.</p>\n' +
+  '\n' +
+  '<p>Medzi interaktÃ­vnymi dialÃ³govÃ½mi komponentmi mÃ´Å¾ete prechÃ¡dzaÅ¥ stlaÄenÃ­m klÃ¡vesov <strong>Tab</strong> alebo <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<h1>NavigÃ¡cia vÂ dialÃ³govÃ½ch oknÃ¡ch so zÃ¡loÅ¾kami</h1>\n' +
+  '\n' +
+  '<p>Pri otvorenÃ­ dialÃ³govÃ½ch okien so zÃ¡loÅ¾kami prejdete na prvÃ© tlaÄidlo vÂ ponuke zÃ¡loÅ¾iek.</p>\n' +
+  '\n' +
+  '<p>Medzi interaktÃ­vnymi komponentmi tejto dialÃ³govej zÃ¡loÅ¾ky mÃ´Å¾ete prechÃ¡dzaÅ¥ stlaÄenÃ­m klÃ¡vesov <strong>Tab</strong> alebo\n' +
+  '  <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Ak chcete prepnÃºÅ¥ na ÄalÅ¡iu zÃ¡loÅ¾ku dialÃ³govÃ©ho okna, prejdite do ponuky zÃ¡loÅ¾iek aÂ potom mÃ´Å¾ete stlaÄenÃ­m prÃ­sluÅ¡nÃ©ho klÃ¡vesu so <strong>Å¡Ã­pkou</strong>\n' +
+  '  prepÃ­naÅ¥ medzi dostupnÃ½mi zÃ¡loÅ¾kami.</p>\n');
