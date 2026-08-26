295f70718c0e3288f4b4b622090b74af7dcc46fb
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:56:06 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/hu_HU.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.hu_HU',
+'<h1>BillentyÅ±zetes navigÃ¡ciÃ³ indÃ­tÃ¡sa</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>FÃ³kusz a menÃ¼sÃ¡vra</dt>\n' +
+  '  <dd>Windows Ã©s Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>FÃ³kusz az eszkÃ¶ztÃ¡rra</dt>\n' +
+  '  <dd>Windows Ã©s Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>FÃ³kusz a lÃ¡blÃ©cre</dt>\n' +
+  '  <dd>Windows Ã©s Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>RÃ¡kÃ¶zelÃ­tÃ©s az Ã©rtesÃ­tÃ©sre</dt>\n' +
+  '  <dd>Windows vagy Linux: Alt+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>FÃ³kusz egy kÃ¶rnyezetfÃ¼ggÅ eszkÃ¶ztÃ¡rra</dt>\n' +
+  '  <dd>Windows, Linux Ã©s macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>A navigÃ¡ciÃ³ az elsÅ felhasznÃ¡lÃ³i felÃ¼leti elemnÃ©l kezdÅdik, amelyet a rendszer kiemel, illetve alÃ¡hÃºz, amennyiben az az elsÅ elem\n' +
+  '  a lÃ¡blÃ©c elemÃºtvonalÃ¡n.</p>\n' +
+  '\n' +
+  '<h1>NavigÃ¡lÃ¡s a felhasznÃ¡lÃ³i felÃ¼let szakaszai kÃ¶zÃ¶tt</h1>\n' +
+  '\n' +
+  '<p>A felhasznÃ¡lÃ³i felÃ¼let kÃ¶vetkezÅ szakaszÃ¡ra vÃ¡ltÃ¡shoz nyomja meg a <strong>Tab</strong> billentyÅ±t.</p>\n' +
+  '\n' +
+  '<p>A felhasznÃ¡lÃ³i felÃ¼let elÅzÅ szakaszÃ¡ra vÃ¡ltÃ¡shoz nyomja meg a <strong>Shift+Tab</strong> billentyÅ±t.</p>\n' +
+  '\n' +
+  '<p>A <strong>Tab</strong> billentyÅ±vel a felhasznÃ¡lÃ³i felÃ¼let szakaszai kÃ¶zÃ¶tt a kÃ¶vetkezÅ sorrendben vÃ¡lt:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>MenÃ¼sÃ¡v</li>\n' +
+  '  <li>Az egyes eszkÃ¶ztÃ¡rcsoportok</li>\n' +
+  '  <li>OldalsÃ¡v</li>\n' +
+  '  <li>ElemÃºtvonal a lÃ¡blÃ©cen</li>\n' +
+  '  <li>SzÃ³szÃ¡mÃ¡tkapcsolÃ³ gomb a lÃ¡blÃ©cen</li>\n' +
+  '  <li>MÃ¡rkalink a lÃ¡blÃ©cen</li>\n' +
+  '  <li>SzerkesztÅ Ã¡tmÃ©retezÃ©si fogÃ³pontja a lÃ¡blÃ©cen</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Ha a felhasznÃ¡lÃ³i felÃ¼let valamelyik eleme nincs jelen, a rendszer kihagyja.</p>\n' +
+  '\n' +
+  '<p>Ha a billentyÅ±zetes navigÃ¡ciÃ³ fÃ³kusza a lÃ¡blÃ©cen van, Ã©s nincs lÃ¡thatÃ³ oldalsÃ¡v, a <strong>Shift+Tab</strong>\n' +
+  '  billentyÅ±kombinÃ¡ciÃ³ lenyomÃ¡sakor az elsÅ eszkÃ¶ztÃ¡rcsoportra ugrik a fÃ³kusz, nem az utolsÃ³ra.</p>\n' +
+  '\n' +
+  '<h1>NavigÃ¡lÃ¡s a felhasznÃ¡lÃ³i felÃ¼let szakaszain belÃ¼l</h1>\n' +
+  '\n' +
+  '<p>A felhasznÃ¡lÃ³i felÃ¼let kÃ¶vetkezÅ elemÃ©re vÃ¡ltÃ¡shoz nyomja meg a megfelelÅ <strong>nyÃ­lbillentyÅ±t</strong>.</p>\n' +
+  '\n' +
+  '<p>A <strong>bal</strong> Ã©s a <strong>jobb</strong> nyÃ­lgomb</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>a menÃ¼sÃ¡vban a menÃ¼k kÃ¶zÃ¶tt vÃ¡lt.</li>\n' +
+  '  <li>a menÃ¼kben megnyit egy almenÃ¼t.</li>\n' +
+  '  <li>az eszkÃ¶ztÃ¡rcsoportban a gombok kÃ¶zÃ¶tt vÃ¡lt.</li>\n' +
+  '  <li>a lÃ¡blÃ©c elemÃºtvonalÃ¡n az elemek kÃ¶zÃ¶tt vÃ¡lt.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>A <strong>le</strong> Ã©s a <strong>fel</strong> nyÃ­lgomb</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>a menÃ¼kben a menÃ¼pontok kÃ¶zÃ¶tt vÃ¡lt.</li>\n' +
+  '  <li>az eszkÃ¶ztÃ¡r elÅugrÃ³ menÃ¼jÃ©ben az elemek kÃ¶zÃ¶tt vÃ¡lt.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>A <strong>nyÃ­lbillentyÅ±k</strong> lenyomÃ¡sÃ¡val kÃ¶rkÃ¶rÃ¶sen lÃ©pkedhet a fÃ³kuszban lÃ©vÅ felhasznÃ¡lÃ³i felÃ¼leti szakasz elemei kÃ¶zÃ¶tt.</p>\n' +
+  '\n' +
+  '<p>A megnyitott menÃ¼ket, almenÃ¼ket Ã©s elÅugrÃ³ menÃ¼ket az <strong>Esc</strong> billentyÅ±vel zÃ¡rhatja be.</p>\n' +
+  '\n' +
+  '<p>Ha a fÃ³kusz az aktuÃ¡lis felÃ¼leti elem âfelsÅâ rÃ©szÃ©n van, az <strong>Esc</strong> billentyÅ±vel az egÃ©sz\n' +
+  '  billentyÅ±zetes navigÃ¡ciÃ³bÃ³l kilÃ©p.</p>\n' +
+  '\n' +
+  '<h1>MenÃ¼pont vagy eszkÃ¶ztÃ¡rgomb aktivÃ¡lÃ¡sa</h1>\n' +
+  '\n' +
+  '<p>Amikor a kÃ­vÃ¡nt menÃ¼elem vagy eszkÃ¶ztÃ¡rgomb van kijelÃ¶lve, nyomja meg a <strong>Return</strong>, az <strong>Enter</strong>\n' +
+  '  vagy a <strong>SzÃ³kÃ¶z</strong> billentyÅ±t az adott elem vagy gomb aktivÃ¡lÃ¡sÃ¡hoz.</p>\n' +
+  '\n' +
+  '<h1>NavigÃ¡lÃ¡s a lapokkal nem rendelkezÅ pÃ¡rbeszÃ©dablakokban</h1>\n' +
+  '\n' +
+  '<p>A lapokkal nem rendelkezÅ pÃ¡rbeszÃ©dablakokban az elsÅ interaktÃ­v Ã¶sszetevÅ kapja a fÃ³kuszt, amikor a pÃ¡rbeszÃ©dpanel megnyÃ­lik.</p>\n' +
+  '\n' +
+  '<p>A pÃ¡rbeszÃ©dpanelek interaktÃ­v Ã¶sszetevÅi kÃ¶zÃ¶tt a <strong>Tab</strong> vagy a <strong>Shift+Tab</strong> billentyÅ±vel navigÃ¡lhat.</p>\n' +
+  '\n' +
+  '<h1>NavigÃ¡lÃ¡s a lapokkal rendelkezÅ pÃ¡rbeszÃ©dablakokban</h1>\n' +
+  '\n' +
+  '<p>A lapokkal rendelkezÅ pÃ¡rbeszÃ©dablakokban a lapmenÃ¼ elsÅ gombja kapja a fÃ³kuszt, amikor a pÃ¡rbeszÃ©dpanel megnyÃ­lik.</p>\n' +
+  '\n' +
+  '<p>A pÃ¡rbeszÃ©dpanel e lapjÃ¡nak interaktÃ­v Ã¶sszetevÅi kÃ¶zÃ¶tt a <strong>Tab</strong> vagy\n' +
+  '  <strong>Shift+Tab</strong> billentyÅ±vel navigÃ¡lhat.</p>\n' +
+  '\n' +
+  '<p>A pÃ¡rbeszÃ©dablak mÃ¡sik lapjÃ¡ra Ãºgy lÃ©phet, hogy a fÃ³kuszt a lapmenÃ¼re Ã¡llÃ­tja, majd lenyomja a megfelelÅ <strong>nyÃ­lbillentyÅ±t</strong>\n' +
+  '  a rendelkezÃ©sre Ã¡llÃ³ lapok kÃ¶zÃ¶tti lÃ©pkedÃ©shez.</p>\n');
