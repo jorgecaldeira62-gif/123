f2f04180ba8f7d81717a5758496688f584c079a4
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:56:23 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/pt-BR.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.pt-BR',
+'<h1>Iniciar navegaÃ§Ã£o pelo teclado</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>Foco na barra de menus</dt>\n' +
+  '  <dd>Windows ou Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>Foco na barra de ferramentas</dt>\n' +
+  '  <dd>Windows ou Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>Foco no rodapÃ©</dt>\n' +
+  '  <dd>Windows ou Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>Foco na notificaÃ§Ã£o</dt>\n' +
+  '  <dd>Windows ou Linux: Alt+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>Foco na barra de ferramentas contextual</dt>\n' +
+  '  <dd>Windows, Linux ou macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>A navegaÃ§Ã£o inicia no primeiro item da IU, que serÃ¡ destacado ou sublinhado no caso do primeiro item no\n' +
+  '  caminho do elemento RodapÃ©.</p>\n' +
+  '\n' +
+  '<h1>Navegar entre seÃ§Ãµes da IU</h1>\n' +
+  '\n' +
+  '<p>Para ir de uma seÃ§Ã£o da IU para a seguinte, pressione <strong>Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Para ir de uma seÃ§Ã£o da IU para a anterior, pressione <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>A ordem de <strong>Tab</strong> destas seÃ§Ãµes da IU Ã©:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Barra de menus</li>\n' +
+  '  <li>Cada grupo da barra de ferramentas</li>\n' +
+  '  <li>Barra lateral</li>\n' +
+  '  <li>Caminho do elemento no rodapÃ©</li>\n' +
+  '  <li>BotÃ£o de alternar contagem de palavras no rodapÃ©</li>\n' +
+  '  <li>Link da marca no rodapÃ©</li>\n' +
+  '  <li>AlÃ§a de redimensionamento do editor no rodapÃ©</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Se nÃ£o houver uma seÃ§Ã£o da IU, ela serÃ¡ pulada.</p>\n' +
+  '\n' +
+  '<p>Se o rodapÃ© tiver o foco da navegaÃ§Ã£o pelo teclado e nÃ£o houver uma barra lateral visÃ­vel, pressionar <strong>Shift+Tab</strong>\n' +
+  '  move o foco para o primeiro grupo da barra de ferramentas, nÃ£o para o Ãºltimo.</p>\n' +
+  '\n' +
+  '<h1>Navegar dentro das seÃ§Ãµes da IU</h1>\n' +
+  '\n' +
+  '<p>Para ir de um elemento da IU para o seguinte, pressione a <strong>Seta</strong> correspondente.</p>\n' +
+  '\n' +
+  '<p>As teclas de seta <strong>Esquerda</strong> e <strong>Direita</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>movem entre menus na barra de menus.</li>\n' +
+  '  <li>abrem um submenu em um menu.</li>\n' +
+  '  <li>movem entre botÃµes em um grupo da barra de ferramentas.</li>\n' +
+  '  <li>movem entre itens no caminho do elemento do rodapÃ©.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>As teclas de seta <strong>Abaixo</strong> e <strong>Acima</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>movem entre itens de menu em um menu.</li>\n' +
+  '  <li>movem entre itens em um menu suspenso da barra de ferramentas.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>As teclas de <strong>Seta</strong> alternam dentre a seÃ§Ã£o da IU em foco.</p>\n' +
+  '\n' +
+  '<p>Para fechar um menu aberto, um submenu aberto ou um menu suspenso aberto, pressione <strong>Esc</strong>.</p>\n' +
+  '\n' +
+  '<p>Se o foco atual estiver no âaltoâ de determinada seÃ§Ã£o da IU, pressionar <strong>Esc</strong> tambÃ©m sai\n' +
+  '  totalmente da navegaÃ§Ã£o pelo teclado.</p>\n' +
+  '\n' +
+  '<h1>Executar um item de menu ou botÃ£o da barra de ferramentas</h1>\n' +
+  '\n' +
+  '<p>Com o item de menu ou botÃ£o da barra de ferramentas desejado destacado, pressione <strong>Return</strong>, <strong>Enter</strong>,\n' +
+  '  ou a <strong>Barra de espaÃ§o</strong> para executar o item.</p>\n' +
+  '\n' +
+  '<h1>Navegar por caixas de diÃ¡logo sem guias</h1>\n' +
+  '\n' +
+  '<p>Em caixas de diÃ¡logo sem guias, o primeiro componente interativo recebe o foco quando a caixa de diÃ¡logo abre.</p>\n' +
+  '\n' +
+  '<p>Navegue entre componentes interativos de caixa de diÃ¡logo pressionando <strong>Tab</strong> ou <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<h1>Navegar por caixas de diÃ¡logo com guias</h1>\n' +
+  '\n' +
+  '<p>Em caixas de diÃ¡logo com guias, o primeiro botÃ£o no menu da guia recebe o foco quando a caixa de diÃ¡logo abre.</p>\n' +
+  '\n' +
+  '<p>Navegue entre componentes interativos dessa guia da caixa de diÃ¡logo pressionando <strong>Tab</strong> ou\n' +
+  '  <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Alterne para outra guia da caixa de diÃ¡logo colocando o foco no menu da guia e pressionando a <strong>Seta</strong>\n' +
+  '  adequada para percorrer as guias disponÃ­veis.</p>\n');
