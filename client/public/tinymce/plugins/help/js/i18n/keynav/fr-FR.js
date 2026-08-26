75a593a19c0bf4f726944f99e716dd7bc1f40ecf
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:55:57 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/fr-FR.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.fr-FR',
+'<h1>DÃ©buter la navigation au clavier</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>Cibler la barre du menu</dt>\n' +
+  '  <dd>Windows ou LinuxÂ : Alt+F9</dd>\n' +
+  '  <dd>macOSÂ : &#x2325;F9</dd>\n' +
+  "  <dt>Cibler la barre d'outils</dt>\n" +
+  '  <dd>Windows ou LinuxÂ : Alt+F10</dd>\n' +
+  '  <dd>macOSÂ : &#x2325;F10</dd>\n' +
+  '  <dt>Cibler le pied de page</dt>\n' +
+  '  <dd>Windows ou LinuxÂ : Alt+F11</dd>\n' +
+  '  <dd>macOSÂ : &#x2325;F11</dd>\n' +
+  '  <dt>Cibler la notification</dt>\n' +
+  '  <dd>Windows ou Linux : Alt+F12</dd>\n' +
+  '  <dd>macOS : &#x2325;F12</dd>\n' +
+  "  <dt>Cibler une barre d'outils contextuelle</dt>\n" +
+  '  <dd>Windows, Linux ou macOSÂ : Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  "<p>La navigation dÃ©butera sur le premier Ã©lÃ©ment de l'interface utilisateur, qui sera mis en surbrillance ou bien soulignÃ© dans le cas du premier Ã©lÃ©ment du\n" +
+  "  chemin d'Ã©lÃ©ments du pied de page.</p>\n" +
+  '\n' +
+  "<h1>Naviguer entre les sections de l'interface utilisateur</h1>\n" +
+  '\n' +
+  "<p>Pour passer d'une section de l'interface utilisateur Ã  la suivante, appuyez sur <strong>Tabulation</strong>.</p>\n" +
+  '\n' +
+  "<p>Pour passer d'une section de l'interface utilisateur Ã  la prÃ©cÃ©dente, appuyez sur <strong>Maj+Tabulation</strong>.</p>\n" +
+  '\n' +
+  "<p>L'ordre de <strong>Tabulation</strong> de ces sections de l'interface utilisateur est le suivantÂ :</p>\n" +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Barre du menu</li>\n' +
+  "  <li>Chaque groupe de barres d'outils</li>\n" +
+  '  <li>Barre latÃ©rale</li>\n' +
+  "  <li>Chemin d'Ã©lÃ©ments du pied de page</li>\n" +
+  "  <li>Bouton d'activation du compteur de mots dans le pied de page</li>\n" +
+  '  <li>Lien de marque dans le pied de page</li>\n' +
+  "  <li>PoignÃ©e de redimensionnement de l'Ã©diteur dans le pied de page</li>\n" +
+  '</ol>\n' +
+  '\n' +
+  "<p>Si une section de l'interface utilisateur n'est pas prÃ©sente, elle sera ignorÃ©e.</p>\n" +
+  '\n' +
+  "<p>Si le pied de page comporte un ciblage par navigation au clavier et qu'il n'y a aucune barre latÃ©rale visible, appuyer sur <strong>Maj+Tabulation</strong>\n" +
+  "  dÃ©place le ciblage vers le premier groupe de barres d'outils et non le dernier.</p>\n" +
+  '\n' +
+  "<h1>Naviguer au sein des sections de l'interface utilisateur</h1>\n" +
+  '\n' +
+  "<p>Pour passer d'un Ã©lÃ©ment de l'interface utilisateur au suivant, appuyez sur la <strong>FlÃ¨che</strong> appropriÃ©e.</p>\n" +
+  '\n' +
+  '<p>Les touches flÃ©chÃ©es <strong>Gauche</strong> et <strong>Droite</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>se dÃ©placent entre les menus de la barre des menus.</li>\n' +
+  "  <li>ouvrent un sous-menu au sein d'un menu.</li>\n" +
+  "  <li>se dÃ©placent entre les boutons d'un groupe de barres d'outils.</li>\n" +
+  "  <li>se dÃ©placent entre les Ã©lÃ©ments du chemin d'Ã©lÃ©ments du pied de page.</li>\n" +
+  '</ul>\n' +
+  '\n' +
+  '<p>Les touches flÃ©chÃ©es <strong>Bas</strong> et <strong>Haut</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  "  <li>se dÃ©placent entre les Ã©lÃ©ments de menu au sein d'un menu.</li>\n" +
+  "  <li>se dÃ©placent entre les Ã©lÃ©ments au sein d'un menu contextuel de barre d'outils.</li>\n" +
+  '</ul>\n' +
+  '\n' +
+  "<p>Les <strong>FlÃ¨ches</strong> parcourent la section de l'interface utilisateur ciblÃ©e.</p>\n" +
+  '\n' +
+  '<p>Pour fermer un menu ouvert, un sous-menu ouvert ou un menu contextuel ouvert, appuyez sur <strong>Echap</strong>.</p>\n' +
+  '\n' +
+  "<p>Si l'actuel ciblage se trouve en Â«Â hautÂ Â» d'une section spÃ©cifique de l'interface utilisateur, appuyer sur <strong>Echap</strong> permet Ã©galement de quitter\n" +
+  '  entiÃ¨rement la navigation au clavier.</p>\n' +
+  '\n' +
+  "<h1>ExÃ©cuter un Ã©lÃ©ment de menu ou un bouton de barre d'outils</h1>\n" +
+  '\n' +
+  "<p>Lorsque l'Ã©lÃ©ment de menu ou le bouton de barre d'outils dÃ©sirÃ© est mis en surbrillance, appuyez sur la touche <strong>Retour arriÃ¨re</strong>, <strong>EntrÃ©e</strong>\n" +
+  "  ou la <strong>Barre d'espace</strong> pour exÃ©cuter l'Ã©lÃ©ment.</p>\n" +
+  '\n' +
+  '<h1>Naviguer au sein de dialogues sans onglets</h1>\n' +
+  '\n' +
+  "<p>Dans les dialogues sans onglets, le premier composant interactif est ciblÃ© lorsque le dialogue s'ouvre.</p>\n" +
+  '\n' +
+  '<p>Naviguez entre les composants du dialogue interactif en appuyant sur <strong>Tabulation</strong> ou <strong>Maj+Tabulation</strong>.</p>\n' +
+  '\n' +
+  '<h1>Naviguer au sein de dialogues avec onglets</h1>\n' +
+  '\n' +
+  "<p>Dans les dialogues avec onglets, le premier bouton du menu de l'onglet est ciblÃ© lorsque le dialogue s'ouvre.</p>\n" +
+  '\n' +
+  '<p>Naviguez entre les composants interactifs de cet onglet de dialogue en appuyant sur <strong>Tabulation</strong> ou\n' +
+  '  <strong>Maj+Tabulation</strong>.</p>\n' +
+  '\n' +
+  "<p>Passez Ã  un autre onglet de dialogue en ciblant le menu de l'onglet et en appuyant sur la <strong>FlÃ¨che</strong>\n" +
+  '  appropriÃ©e pour parcourir les onglets disponibles.</p>\n');
