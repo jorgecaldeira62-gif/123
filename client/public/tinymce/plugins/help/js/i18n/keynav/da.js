38ca58c0e50c7df59851c90ab807a867938398b9
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:55:45 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/da.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.da',
+'<h1>Start tastaturnavigation</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>Fokuser pÃ¥ menulinjen</dt>\n' +
+  '  <dd>Windows eller Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>Fokuser pÃ¥ vÃ¦rktÃ¸jslinjen</dt>\n' +
+  '  <dd>Windows eller Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>Fokuser pÃ¥ sidefoden</dt>\n' +
+  '  <dd>Windows eller Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>Fokuser pÃ¥ meddelelsen</dt>\n' +
+  '  <dd>Windows eller Linux: Alt+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>Fokuser pÃ¥ kontekstuel vÃ¦rktÃ¸jslinje</dt>\n' +
+  '  <dd>Windows, Linux eller macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>Navigationen starter ved det fÃ¸rste UI-element, som fremhÃ¦ves eller understreges hvad angÃ¥r det fÃ¸rste element i\n' +
+  '  sidefodens sti til elementet.</p>\n' +
+  '\n' +
+  '<h1>Naviger mellem UI-sektioner</h1>\n' +
+  '\n' +
+  '<p>GÃ¥ fra Ã©n UI-sektion til den nÃ¦ste ved at trykke pÃ¥ <strong>Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>GÃ¥ fra Ã©n UI-sektion til den forrige ved at trykke pÃ¥ <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p><strong>Tab</strong>-rÃ¦kkefÃ¸lgen af disse UI-sektioner er:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Menulinje</li>\n' +
+  '  <li>Hver vÃ¦rktÃ¸jsgruppe</li>\n' +
+  '  <li>Sidepanel</li>\n' +
+  '  <li>Sti til elementet i sidefoden</li>\n' +
+  '  <li>Til/fra-knap for ordoptÃ¦lling i sidefoden</li>\n' +
+  '  <li>Brandinglink i sidefoden</li>\n' +
+  '  <li>TilpasningshÃ¥ndtag for editor i sidefoden</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Hvis en UI-sektion ikke er til stede, springes den over.</p>\n' +
+  '\n' +
+  '<p>Hvis sidefoden har fokus til tastaturnavigation, og der ikke er noget synligt sidepanel, kan der trykkes pÃ¥ <strong>Shift+Tab</strong>\n' +
+  '  for at flytte fokus til den fÃ¸rste vÃ¦rktÃ¸jsgruppe, ikke den sidste.</p>\n' +
+  '\n' +
+  '<h1>Naviger inden for UI-sektioner</h1>\n' +
+  '\n' +
+  '<p>GÃ¥ fra Ã©t UI-element til det nÃ¦ste ved at trykke pÃ¥ den relevante <strong>piletast</strong>.</p>\n' +
+  '\n' +
+  '<p><strong>Venstre</strong> og <strong>hÃ¸jre</strong> piletast</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>flytter mellem menuerne i menulinjen.</li>\n' +
+  '  <li>Ã¥bner en undermenu i en menu.</li>\n' +
+  '  <li>flytter mellem knapperne i en vÃ¦rktÃ¸jsgruppe.</li>\n' +
+  '  <li>flytter mellem elementer i sidefodens sti til elementet.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>Pil <strong>ned</strong> og <strong>op</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>flytter mellem menupunkterne i en menu.</li>\n' +
+  '  <li>flytter mellem punkterne i en genvejsmenu i vÃ¦rktÃ¸jslinjen.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p><strong>Piletasterne</strong> kÃ¸rer rundt inden for UI-sektionen, der fokuseres pÃ¥.</p>\n' +
+  '\n' +
+  '<p>For at lukke en Ã¥ben menu, en Ã¥ben undermenu eller en Ã¥ben genvejsmenu trykkes der pÃ¥ <strong>Esc</strong>-tasten.</p>\n' +
+  '\n' +
+  "<p>Hvis det aktuelle fokus er i 'toppen' af en bestemt UI-sektion, vil tryk pÃ¥ <strong>Esc</strong>-tasten ogsÃ¥ afslutte\n" +
+  '  tastaturnavigationen helt.</p>\n' +
+  '\n' +
+  '<h1>UdfÃ¸r et menupunkt eller en vÃ¦rktÃ¸jslinjeknap</h1>\n' +
+  '\n' +
+  '<p>NÃ¥r det Ã¸nskede menupunkt eller den Ã¸nskede vÃ¦rktÃ¸jslinjeknap er fremhÃ¦vet, trykkes der pÃ¥ <strong>Retur</strong>, <strong>Enter</strong>\n' +
+  '  eller <strong>mellemrumstasten</strong> for at udfÃ¸re elementet.</p>\n' +
+  '\n' +
+  '<h1>Naviger i ikke-faneopdelte dialogbokse</h1>\n' +
+  '\n' +
+  '<p>I ikke-faneopdelte dialogbokse fÃ¥r den fÃ¸rste interaktive komponent fokus, nÃ¥r dialogboksen Ã¥bnes.</p>\n' +
+  '\n' +
+  '<p>Naviger mellem interaktive dialogbokskomponenter ved at trykke pÃ¥ <strong>Tab</strong> eller <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<h1>Naviger i faneopdelte dialogbokse</h1>\n' +
+  '\n' +
+  '<p>I faneopdelte dialogbokse fÃ¥r den fÃ¸rste knap i fanemenuen fokus, nÃ¥r dialogboksen Ã¥bnes.</p>\n' +
+  '\n' +
+  '<p>Naviger mellem interaktive komponenter i denne dialogboksfane ved at trykke pÃ¥ <strong>Tab</strong> eller\n' +
+  '  <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Skift til en anden dialogboksfane ved at fokusere pÃ¥ fanemenuen og derefter trykke pÃ¥ den relevante <strong>piletast</strong>\n' +
+  '  for at kÃ¸re igennem de tilgÃ¦ngelige faner.</p>\n');
