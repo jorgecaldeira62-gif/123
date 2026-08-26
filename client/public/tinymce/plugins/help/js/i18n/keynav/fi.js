4ebe5247f2a12e01fd7d0a1a2f91d0a2a74ff725
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:55:55 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/fi.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.fi',
+'<h1>NÃ¤ppÃ¤imistÃ¶navigoinnin aloittaminen</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>SiirrÃ¤ kohdistus valikkopalkkiin</dt>\n' +
+  '  <dd>Windows tai Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>SiirrÃ¤ kohdistus tyÃ¶kalupalkkiin</dt>\n' +
+  '  <dd>Windows tai Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>SiirrÃ¤ kohdistus alatunnisteeseen</dt>\n' +
+  '  <dd>Windows tai Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>KeskitÃ¤ ilmoitukseen</dt>\n' +
+  '  <dd>Windows ja Linux: Alt + F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>SiirrÃ¤ kohdistus kontekstuaaliseen tyÃ¶kalupalkkiin</dt>\n' +
+  '  <dd>Windows, Linux tai macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>Navigointi aloitetaan ensimmÃ¤isestÃ¤ kÃ¤yttÃ¶liittymÃ¤n kohteesta, joka joko korostetaan tai alleviivataan, jos\n' +
+  '  kyseessÃ¤ on Alatunniste-elementin polun ensimmÃ¤inen kohde.</p>\n' +
+  '\n' +
+  '<h1>KÃ¤yttÃ¶liittymÃ¤n eri osien vÃ¤lillÃ¤ navigointi</h1>\n' +
+  '\n' +
+  '<p>Paina <strong>sarkainnÃ¤ppÃ¤intÃ¤</strong> siirtyÃ¤ksesi kÃ¤yttÃ¶liittymÃ¤n osasta seuraavaan.</p>\n' +
+  '\n' +
+  '<p>Jos haluat siirtyÃ¤ edelliseen kÃ¤yttÃ¶liittymÃ¤n osaan, paina <strong>Shift+sarkainnÃ¤ppÃ¤in</strong>.</p>\n' +
+  '\n' +
+  '<p><strong>SarkainnÃ¤ppÃ¤in</strong> siirtÃ¤Ã¤ sinua nÃ¤issÃ¤ kÃ¤yttÃ¶liittymÃ¤n osissa tÃ¤ssÃ¤ jÃ¤rjestyksessÃ¤:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Valikkopalkki</li>\n' +
+  '  <li>TyÃ¶kalupalkin ryhmÃ¤t</li>\n' +
+  '  <li>Sivupalkki</li>\n' +
+  '  <li>Elementin polku alatunnisteessa</li>\n' +
+  '  <li>Sanalaskurin vaihtopainike alatunnisteessa</li>\n' +
+  '  <li>BrÃ¤ndÃ¤yslinkki alatunnisteessa</li>\n' +
+  '  <li>Editorin koon muuttamisen kahva alatunnisteessa</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Jos jotakin kÃ¤yttÃ¶liittymÃ¤n osaa ei ole, se ohitetaan.</p>\n' +
+  '\n' +
+  '<p>Jos kohdistus on siirretty alatunnisteeseen nÃ¤ppÃ¤imistÃ¶navigoinnilla eikÃ¤ sivupalkkia ole nÃ¤kyvissÃ¤, <strong>Shift+sarkainnÃ¤ppÃ¤in</strong>\n' +
+  '  siirtÃ¤Ã¤ kohdistuksen tyÃ¶kalupalkin ensimmÃ¤iseen ryhmÃ¤Ã¤n, eikÃ¤ viimeiseen.</p>\n' +
+  '\n' +
+  '<h1>KÃ¤yttÃ¶liittymÃ¤n eri osien sisÃ¤llÃ¤Â navigointi</h1>\n' +
+  '\n' +
+  '<p>Paina <strong>nuolinÃ¤ppÃ¤imiÃ¤</strong> siirtyÃ¤ksesi kÃ¤yttÃ¶liittymÃ¤elementistÃ¤ seuraavaan.</p>\n' +
+  '\n' +
+  '<p><strong>Vasen</strong>- ja <strong>Oikea</strong>-nuolinÃ¤ppÃ¤imet</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>siirtÃ¤vÃ¤t sinua valikkopalkin valikoiden vÃ¤lillÃ¤.</li>\n' +
+  '  <li>avaavat valikon alavalikon.</li>\n' +
+  '  <li>siirtÃ¤vÃ¤t sinua tyÃ¶kalupalkin ryhmÃ¤n painikkeiden vÃ¤lillÃ¤.</li>\n' +
+  '  <li>siirtÃ¤vÃ¤t sinua kohteiden vÃ¤lillÃ¤ alatunnisteen elementin polussa.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p><strong>Alas</strong>- ja <strong>YlÃ¶s</strong>-nuolinÃ¤ppÃ¤imet</p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>siirtÃ¤vÃ¤t sinua valikon valikkokohteiden vÃ¤lillÃ¤.</li>\n' +
+  '  <li>siirtÃ¤vÃ¤t sinua tyÃ¶kalupalkin ponnahdusvalikon kohteiden vÃ¤lillÃ¤.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p><strong>NuolinÃ¤ppÃ¤imet</strong> siirtÃ¤vÃ¤t sinua kÃ¤yttÃ¶liittymÃ¤n korostetun osan sisÃ¤llÃ¤ syklissÃ¤.</p>\n' +
+  '\n' +
+  '<p>Paina <strong>Esc</strong>-nÃ¤ppÃ¤intÃ¤ sulkeaksesi avoimen valikon, avataksesi alavalikon tai avataksesi ponnahdusvalikon.</p>\n' +
+  '\n' +
+  '<p>Jos kohdistus on kÃ¤yttÃ¶liittymÃ¤n tietyn osion ylÃ¤laidassa, <strong>Esc</strong>-nÃ¤ppÃ¤imen painaminen\n' +
+  '  poistuu myÃ¶s nÃ¤ppÃ¤imistÃ¶navigoinnista kokonaan.</p>\n' +
+  '\n' +
+  '<h1>Suorita valikkokohde tai tyÃ¶kalupalkin painike</h1>\n' +
+  '\n' +
+  '<p>Kun haluamasi valikkokohde tai tyÃ¶kalupalkin painike on korostettuna, paina <strong>Return</strong>-, <strong>Enter</strong>-\n' +
+  '  tai <strong>vÃ¤lilyÃ¶ntinÃ¤ppÃ¤intÃ¤</strong> suorittaaksesi kohteen.</p>\n' +
+  '\n' +
+  '<h1>VÃ¤lilehdittÃ¶missÃ¤ valintaikkunoissa navigointi</h1>\n' +
+  '\n' +
+  '<p>Kun vÃ¤lilehdetÃ¶n valintaikkuna avautuu, kohdistus siirtyy sen ensimmÃ¤iseen interaktiiviseen komponenttiin.</p>\n' +
+  '\n' +
+  '<p>Voit siirtyÃ¤ valintaikkunan interaktiivisten komponenttien vÃ¤lillÃ¤ painamalla <strong>sarkainnÃ¤ppÃ¤intÃ¤</strong> tai <strong>Shift+sarkainnÃ¤ppÃ¤in</strong>.</p>\n' +
+  '\n' +
+  '<h1>VÃ¤lilehdellisissÃ¤ valintaikkunoissa navigointi</h1>\n' +
+  '\n' +
+  '<p>Kun vÃ¤lilehdellinen valintaikkuna avautuu, kohdistus siirtyy vÃ¤lilehtivalikon ensimmÃ¤iseen painikkeeseen.</p>\n' +
+  '\n' +
+  '<p>Voit siirtyÃ¤ valintaikkunan vÃ¤lilehden interaktiivisen komponenttien vÃ¤lillÃ¤ painamalla <strong>sarkainnÃ¤ppÃ¤intÃ¤</strong> tai\n' +
+  '  <strong>Shift+sarkainnÃ¤ppÃ¤in</strong>.</p>\n' +
+  '\n' +
+  '<p>Voit siirtyÃ¤ valintaikkunan toiseen vÃ¤lilehteen siirtÃ¤mÃ¤llÃ¤ kohdistuksen vÃ¤lilehtivalikkoon ja painamalla sopivaa <strong>nuolinÃ¤ppÃ¤intÃ¤</strong>\n' +
+  '  siirtyÃ¤ksesi kÃ¤ytettÃ¤vissÃ¤ olevien vÃ¤lilehtien vÃ¤lillÃ¤ syklissÃ¤.</p>\n');
