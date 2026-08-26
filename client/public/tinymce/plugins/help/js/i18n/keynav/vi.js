058ab0a0099b2926e161ff98e5cd1211df559ac1
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-08-26 12:56:43 -0300

    Enviado pelo SK Code Editor - 26/08/2026

---
+++ client/public/tinymce/plugins/help/js/i18n/keynav/vi.js
@@ -0,0 +1,93 @@
+tinymce.Resource.add('tinymce.html-i18n.help-keynav.vi',
+'<h1>Báº¯t Äáº§u Äiá»u hÆ°á»ng bÃ n phÃ­m</h1>\n' +
+  '\n' +
+  '<dl>\n' +
+  '  <dt>Táº­p trung vÃ o thanh menu</dt>\n' +
+  '  <dd>Windows hoáº·c Linux: Alt+F9</dd>\n' +
+  '  <dd>macOS: &#x2325;F9</dd>\n' +
+  '  <dt>Táº­p trung vÃ o thanh cÃ´ng cá»¥</dt>\n' +
+  '  <dd>Windows hoáº·c Linux: Alt+F10</dd>\n' +
+  '  <dd>macOS: &#x2325;F10</dd>\n' +
+  '  <dt>Táº­p trung vÃ o chÃ¢n trang</dt>\n' +
+  '  <dd>Windows hoáº·c Linux: Alt+F11</dd>\n' +
+  '  <dd>macOS: &#x2325;F11</dd>\n' +
+  '  <dt>Táº­p trung vÃ o thÃ´ng bÃ¡o</dt>\n' +
+  '  <dd>Windows hoáº·c Linux: Alt+F12</dd>\n' +
+  '  <dd>macOS: &#x2325;F12</dd>\n' +
+  '  <dt>Táº­p trung vÃ o thanh cÃ´ng cá»¥ ngá»¯ cáº£nh</dt>\n' +
+  '  <dd>Windows, Linux hoáº·c macOS: Ctrl+F9</dd>\n' +
+  '</dl>\n' +
+  '\n' +
+  '<p>Äiá»u hÆ°á»ng sáº½ báº¯t Äáº§u tá»« má»¥c UI Äáº§u tiÃªn. Má»¥c nÃ y sáº½ ÄÆ°á»£c tÃ´ sÃ¡ng hoáº·c cÃ³ gáº¡ch dÆ°á»i (náº¿u lÃ  má»¥c Äáº§u tiÃªn trong\n' +
+  '  ÄÆ°á»ng dáº«n pháº§n tá»­ ChÃ¢n trang).</p>\n' +
+  '\n' +
+  '<h1>Di chuyá»n qua láº¡i giá»¯a cÃ¡c pháº§n UI</h1>\n' +
+  '\n' +
+  '<p>Äá» di chuyá»n tá»« má»t pháº§n UI sang pháº§n tiáº¿p theo, áº¥n <strong>Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Äá» di chuyá»n tá»« má»t pháº§n UI vá» pháº§n trÆ°á»c ÄÃ³, áº¥n <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Thá»© tá»± <strong>Tab</strong> cá»§a cÃ¡c pháº§n UI nÃ y nhÆ° sau:</p>\n' +
+  '\n' +
+  '<ol>\n' +
+  '  <li>Thanh menu</li>\n' +
+  '  <li>Tá»«ng nhÃ³m thanh cÃ´ng cá»¥</li>\n' +
+  '  <li>Thanh bÃªn</li>\n' +
+  '  <li>ÄÆ°á»ng dáº«n pháº§n tá»­ trong chÃ¢n trang</li>\n' +
+  '  <li>NÃºt chuyá»n Äá»i Äáº¿m chá»¯ á» chÃ¢n trang</li>\n' +
+  '  <li>LiÃªn káº¿t thÆ°Æ¡ng hiá»u á» chÃ¢n trang</li>\n' +
+  '  <li>NÃºm Äiá»u tÃ¡c chá»nh kÃ­ch cá»¡ trÃ¬nh soáº¡n tháº£o á» chÃ¢n trang</li>\n' +
+  '</ol>\n' +
+  '\n' +
+  '<p>Náº¿u ngÆ°á»i dÃ¹ng khÃ´ng tháº¥y má»t pháº§n UI, thÃ¬ cÃ³ nghÄ©a pháº§n ÄÃ³ bá» bá» qua.</p>\n' +
+  '\n' +
+  '<p>Náº¿u á» chÃ¢n trang cÃ³ tÃ­nh nÄng táº­p trung Äiá»u hÆ°á»ng bÃ n phÃ­m, mÃ  khÃ´ng cÃ³ thanh bÃªn nÃ o hiá»n há»¯u, thao tÃ¡c áº¥n <strong>Shift+Tab</strong>\n' +
+  '  sáº½ chuyá»n hÆ°á»ng táº­p trung vÃ o nhÃ³m thanh cÃ´ng cá»¥ Äáº§u tiÃªn, khÃ´ng pháº£i cuá»i cÃ¹ng.</p>\n' +
+  '\n' +
+  '<h1>Di chuyá»n qua láº¡i trong cÃ¡c pháº§n UI</h1>\n' +
+  '\n' +
+  '<p>Äá» di chuyá»n tá»« má»t pháº§n tá»­ UI sang pháº§n tiáº¿p theo, áº¥n phÃ­m <strong>MÅ©i tÃªn</strong> tÆ°Æ¡ng á»©ng cho phÃ¹ há»£p.</p>\n' +
+  '\n' +
+  '<p>CÃ¡c phÃ­m mÅ©i tÃªn <strong>TrÃ¡i</strong> vÃ  <strong>Pháº£i</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>di chuyá»n giá»¯a cÃ¡c menu trong thanh menu.</li>\n' +
+  '  <li>má» menu phá»¥ trong má»t menu.</li>\n' +
+  '  <li>di chuyá»n giá»¯a cÃ¡c nÃºt trong nhÃ³m thanh cÃ´ng cá»¥.</li>\n' +
+  '  <li>di chuyá»n giá»¯a cÃ¡c má»¥c trong ÄÆ°á»ng dáº«n pháº§n tá»­ cá»§a chÃ¢n trang.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>CÃ¡c phÃ­m mÅ©i tÃªn <strong>HÆ°á»ng xuá»ng</strong> vÃ  <strong>HÆ°á»ng lÃªn</strong></p>\n' +
+  '\n' +
+  '<ul>\n' +
+  '  <li>di chuyá»n giá»¯a cÃ¡c má»¥c menu trong menu.</li>\n' +
+  '  <li>di chuyá»n giá»¯a cÃ¡c má»¥c trong menu thanh cÃ´ng cá»¥ dáº¡ng báº­t lÃªn.</li>\n' +
+  '</ul>\n' +
+  '\n' +
+  '<p>CÃ¡c phÃ­m <strong>mÅ©i tÃªn</strong> xoay vÃ²ng trong má»t pháº§n UI táº­p trung.</p>\n' +
+  '\n' +
+  '<p>Äá» ÄÃ³ng má»t menu má», má»t menu phá»¥ Äang má», hoáº·c má»t menu dáº¡ng báº­t lÃªn Äang má», hÃ£y áº¥n phÃ­m <strong>Esc</strong>.</p>\n' +
+  '\n' +
+  '<p>Náº¿u trá»ng tÃ¢m hiá»n táº¡i lÃ  á» pháº§n âÄáº§uâ cá»§a má»t pháº§n UI cá»¥ thá», thao tÃ¡c áº¥n phÃ­m <strong>Esc</strong> cÅ©ng sáº½ thoÃ¡t\n' +
+  '  toÃ n bá» pháº§n Äiá»u hÆ°á»ng bÃ n phÃ­m.</p>\n' +
+  '\n' +
+  '<h1>Thá»±c hiá»n chá»©c nÄng cá»§a má»t má»¥c menu hoáº·c nÃºt thanh cÃ´ng cá»¥</h1>\n' +
+  '\n' +
+  '<p>Khi má»¥c menu hoáº·c nÃºt thanh cÃ´ng cá»¥ muá»n dÃ¹ng ÄÆ°á»£c tÃ´ sÃ¡ng, hÃ£y áº¥n <strong>Return</strong>, <strong>Enter</strong>,\n' +
+  '  hoáº·c <strong>PhÃ­m cÃ¡ch</strong> Äá» thá»±c hiá»n chá»©c nÄng má»¥c ÄÃ³.</p>\n' +
+  '\n' +
+  '<h1>Äiá»u hÆ°á»ng giá»¯a cÃ¡c há»p thoáº¡i khÃ´ng cÃ³ nhiá»u tab</h1>\n' +
+  '\n' +
+  '<p>Trong cÃ¡c há»p thoáº¡i khÃ´ng cÃ³ nhiá»u tab, khi há»p thoáº¡i má» ra, trá»ng tÃ¢m sáº½ hÆ°á»ng vÃ o thÃ nh pháº§n tÆ°Æ¡ng tÃ¡c Äáº§u tiÃªn.</p>\n' +
+  '\n' +
+  '<p>Di chuyá»n giá»¯a cÃ¡c thÃ nh pháº§n há»p thoáº¡i tÆ°Æ¡ng tÃ¡c báº±ng cÃ¡ch áº¥n <strong>Tab</strong> hoáº·c <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<h1>Äiá»u hÆ°á»ng giá»¯a cÃ¡c há»p thoáº¡i cÃ³ nhiá»u tab</h1>\n' +
+  '\n' +
+  '<p>Trong cÃ¡c há»p thoáº¡i cÃ³ nhiá»u tab, khi há»p thoáº¡i má» ra, trá»ng tÃ¢m sáº½ hÆ°á»ng vÃ o nÃºt Äáº§u tiÃªn trong menu tab.</p>\n' +
+  '\n' +
+  '<p>Di chuyá»n giá»¯a cÃ¡c thÃ nh pháº§n tÆ°Æ¡ng tÃ¡c cá»§a tab há»p thoáº¡i nÃ y báº±ng cÃ¡ch áº¥n <strong>Tab</strong> hoáº·c\n' +
+  '  <strong>Shift+Tab</strong>.</p>\n' +
+  '\n' +
+  '<p>Chuyá»n sang má»t tab há»p thoáº¡i khÃ¡c báº±ng cÃ¡ch chuyá»n trá»ng tÃ¢m vÃ o menu tab, rá»i áº¥n phÃ­m <strong>MÅ©i tÃªn</strong> phÃ¹ há»£p\n' +
+  '  Äá» xoay vÃ²ng cÃ¡c tab hiá»n cÃ³.</p>\n');
